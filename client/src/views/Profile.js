b198c97126934a0cc2ef16a0745e95e61c5a2d60
bathtaters <nomoremrnice7@gmail.com>
2021-10-19 15:41:00 -0400

    remove accounts

--- client/src/views/Profile.js
+++ client/src/views/Profile.js
@@ -3,38 +3,27 @@ import { useParams } from "react-router-dom";
 import {ReactComponent as ProfilePic} from "../assets/blank-user.svg";
 import PropTypes from 'prop-types';
 
-function Profile(props) {
+function Profile({ data }) {
   let { id } = useParams();
-  if (!id) id = props.activeUser; 
-  const edit = props.activeUser === id ? "" : "hidden";
   return pug`
     h3.font-thin User Profile
     .flex
       div
-        ProfilePic.w-36.h-40.bg-gray-400.m-2.inline-block()
-        .text-center.text-xs.italic.text-gray-600.font-light= 'id: ' + id
+        ProfilePic.w-36.h-40.bg-gray-500.m-2.inline-block()
+        .text-center.text-xs.italic.dim-color.font-light= 'id: ' + id
 
       div
-        .grid.grid-flow-row.gap-x-2.gap-y-1(className="grid-cols-"+(props.activeUser === id ? 3 : 2))
-          h4.text-right.text-gray-200.font-thin Name
+        .grid.grid-flow-row.gap-x-2.gap-y-1.grid-cols-3.items-baseline
+          h4.text-right.base-color.font-thin Name
 
-          h4.text-gray-200= props.data[id].name
+          h4.base-color= data[id].name
 
-          a.text-right.font-light.text-sm(href="#change-name" className=edit) Change
-
-          .text-right.text-sm.font-thin.mr-1 Username
-
-          .text-sm= props.data[id].username
-
-          a.text-right.font-light.text-sm(href="#change-un" className=edit) Change
-
-          a.col-span-3.font-light.text-xs.italic.text-center.mt-2(href="#change-pw" className=edit) Change Password
+          a.text-right.font-light.text-xs(href="#change-name") change
   `;
 }
 
 Profile.propTypes = {
   data: PropTypes.object,
-  activeUser: PropTypes.string,
 };
 
 export default Profile;
