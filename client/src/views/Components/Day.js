7c2282e909ed425a12840d1739905c361595e46c
bathtaters <nomoremrnice7@gmail.com>
2021-10-30 04:26:35 -0400

    implement editing functions

--- client/src/views/Components/Day.js
+++ client/src/views/Components/Day.js
@@ -1,37 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
-import { weekdays } from '../assets/strings';
-import { sameDay } from "../controllers/getDays";
+import { weekdays } from '../../assets/strings';
+import { sameDay } from "../../controllers/getDays";
 
-function Day({ data, drafts }) {
+import dragHandle, { preventDef } from '../../controllers/dragAndDrop';
+
+// Highlight on drag class style
+const highlightCss = ['border-double','bg-opacity-50','max-border'];
+
+function Day({ data, drafts, isEditing, swapDrafts, setDraftModal }) {
   const today = new Date();
   const isToday = sameDay(data.day, today);
-  const titleCls = isToday ? "bright-color" : data.day && data.day > today ? "base-color" : "dim-color";
+  const titleCls = isToday ? "max-color" : data.day && data.day > today ? "base-color" : "dim-color-inv";
+  const borderCls = isToday ? "pos-border" : data.day && data.day > today ? "base-border" : "dimmer-border";
   
+  const editDraft = draftId => () => setDraftModal(draftId);
+
   return pug`
-    .p-2.m-1.border.rounded-md.w-40.min-h-32(className="border-gray-" + (isToday ? "800" : "4 00")+" dark:border-gray-" + (isToday ? "200" : "600"))
-      h4.text-2xl.font-light.text-center(className=titleCls)= data.day ? weekdays[data.day.getDay()] : 'Unscheduled'
+    .p-2.m-1.border.rounded-md.w-40.min-h-32.max-bgd.bg-opacity-0.border-opacity-100(
+      className=(isEditing ? "border-dashed dim-border" : borderCls)
+      onDragEnter=dragHandle.enter(highlightCss)
+      onDragOver=preventDef
+      onDragLeave=dragHandle.leave(highlightCss)
+      onDrop=dragHandle.drop({ day: data.day }, swapDrafts, highlightCss)
+    )
+      h4.text-2xl.font-light.text-center.pointer-events-none(
+        className=titleCls
+      )= data.day ? weekdays[data.day.getDay()] : 'Unscheduled'
 
-      h5.text-center.italic.text-sm.font-thin.mb-2= data.day ? data.day.toLocaleDateString() : ''
+      h5.text-center.italic.text-sm.font-thin.mb-2.pointer-events-none= data.day ? data.day.toLocaleDateString() : ''
 
-      if data.drafts
-        ul.list-disc.list-inside.dim-color
-          each draftId in data.drafts
-            li.overflow-ellipsis.whitespace-nowrap.overflow-hidden(key=draftId)
-              Link.text-sm.font-normal(
-                to="/draft/"+draftId,
-                className=(drafts[draftId].isDone ? "dim-color" : ""),
+      if data.drafts && data.drafts.length
+        each draftId in data.drafts
+          .relative.border.border-dashed.rounded-xl.dimmer-border.border-opacity-0.m-1.p-1.max-bgd.bg-opacity-0.text-center(
+            className=(isEditing ? "border-opacity-100 hover:bg-opacity-50" : "")
+            draggable=isEditing
+            onDragStart=dragHandle.start({ id: draftId, day: data.day })
+            onDragEnter=dragHandle.enter(highlightCss)
+            onDragOver=preventDef
+            onDragLeave=dragHandle.leave(highlightCss)
+            onDrop=dragHandle.drop({ id: draftId, day: data.day }, swapDrafts, highlightCss)
+            key=draftId
+          )
+            if isEditing
+              .text-sm.font-normal.pointer-events-none(
+                className=(drafts[draftId].isDone ? "dim-color" : "link-color")
               )= drafts[draftId].title
 
-      else
-        div.text-center.text-sm.font-light.dim-color.italic No drafts
+              .absolute.top-0.right-1.text-sm.font-normal.cursor-pointer(
+                className="hover:pos-color"
+                onClick=editDraft(draftId)
+              ) ✐
+
+            else
+              Link.text-sm.font-normal.block(
+                to="/draft/"+draftId
+                className=(drafts[draftId].isDone ? "dim-color" : "")
+              )= drafts[draftId].title
+                
+      else if !isEditing
+        .text-center.text-sm.font-light.dim-color.italic.pointer-events-none.opacity-60 No drafts
   `;
 }
 
 Day.propTypes = {
   data: PropTypes.object,
   drafts: PropTypes.object,
+  isEditing: PropTypes.bool,
+  swapDrafts: PropTypes.func,
+  setDraftModal: PropTypes.func,
 };
 
 export default Day;
--- client/src/views/Schedule.js
+++ client/src/views/Schedule.js
@@ -1,23 +1,134 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
 import PropTypes from 'prop-types';
-import Day from "./Components/Day";
-import getDays from "../controllers/getDays";
+import Modal from "./Components/Modal";
+import Day from './Components/Day';
+import EditDraft from './Components/EditDraft';
 
-function Schedule({ schedule, drafts, range }) {
-  const dayArray = getDays(range, schedule);
+import getDays, { sameDay } from '../controllers/getDays';
+import { swapData } from '../controllers/swapData';
+
+function Schedule({ schedule, range, drafts, players }) {
+  const modal = useRef(null);
+  const [draftData, setDrafts] = useState(JSON.parse(JSON.stringify(drafts)));
+  const [playerData, setPlayers] = useState(JSON.parse(JSON.stringify(players)));
+  const [scheduleData, setSchedule] = useState(getDays(range, schedule));
+  const [isEditing, setEdit] = useState(false);
+  const [currentDraft, setCurrentDraft] = useState(null);
+
+  const openDraftModal = useCallback(draftId => { setCurrentDraft(draftId); modal.current.open(); }, [modal]);
+
+  const resetSchedule = useCallback(() => { setSchedule(getDays(range, schedule)); setEdit(false); }, [range,schedule]);
+
+  const swapDrafts = useCallback((draftA, draftB) => {
+    if (draftA.id === draftB.id ||
+      (!draftB.id && sameDay(draftA.day, draftB.day))) return;
+    setSchedule(swapData(
+      scheduleData, 'drafts', 'day', draftA, draftB, 'id',
+      (a,b) => a ? sameDay(a,b) : a===b
+    ));
+  }, [scheduleData]);
+
+  const editDraft = draftId => (draftSettings, newPlayers=null) => {
+    // Update new player names to playerIds
+    if (newPlayers && newPlayers.length) {
+      const newPlayerData = getPlayers(newPlayers, playerData);
+      draftSettings.ranking = updateArray(draftSettings.ranking, flipObject(newPlayers, newPlayerData, 'name'));
+      setPlayers(newPlayerData);
+    }
+    // Create new draft & add to "Unscheduled"
+    let updateSched;
+    if (!draftId) {
+      draftId = newId('d', draftData);
+      updateSched = [
+        scheduleData[0].day ?
+          { day: null, drafts: [draftId] } :
+          { ...scheduleData[0], drafts: scheduleData[0].drafts.concat(draftId)},
+        ...scheduleData.slice(scheduleData[0].day ? 0 : 1)
+      ];
+    }
+    // Add draft & close modal
+    setDrafts({ ...draftData, [draftId]: draftSettings });
+    if (updateSched) setSchedule(updateSched);
+    modal.current.close(true);
+  };
+
   return pug`
     div
-      h2.text-center.font-thin Schedule
+      .flex.justify-evenly.items-center
+        input.font-light.dim-color.w-14.h-8(
+          className="sm:w-20 sm:h-11"
+          type="button"
+          value=(isEditing ? "Revert" : "+")
+          onClick=(isEditing ? resetSchedule : ()=>openDraftModal())
+        )
+        
+        h2.inline-block.text-center.font-thin Schedule
+        
+        input.font-light.dim-color.w-14.h-8(
+          className="sm:w-20 sm:h-11"
+          type="button"
+          value=(isEditing ? "Save" : "Edit")
+          onClick=(()=>setEdit(!isEditing))
+        )
+
       .flex.flex-wrap.justify-center.mt-4
-        each dayBlock in dayArray
-          Day(data=dayBlock drafts=drafts key=dayBlock.day ? dayBlock.day.toISOString() : 'NULL')
+        each dayBlock in scheduleData
+          Day(
+            data=dayBlock
+            drafts=draftData
+            isEditing=isEditing
+            swapDrafts=swapDrafts
+            setDraftModal=openDraftModal
+            key=(dayBlock.day ? dayBlock.day.toISOString() : 'NULL')
+          )
+      
+      Modal(ref=modal, startLocked=true)
+        EditDraft(
+          draft=draftData[currentDraft]
+          players=playerData
+          editDraft=editDraft(currentDraft)
+          hideModal=(force=>modal.current.close(force))
+        )
   `;
 }
 
 Schedule.propTypes = {
   schedule: PropTypes.arrayOf(PropTypes.object),
-  drafts: PropTypes.object,
   range: PropTypes.arrayOf(PropTypes.object),
+  drafts: PropTypes.object,
+  players: PropTypes.object,
 };
 
 export default Schedule;
+
+const flipObject = (keyArray, obj, matchOn=null) => keyArray.reduce((res,key) => {
+  res[key] = Object.keys(obj).find(k => (matchOn ? obj[k][matchOn] : obj[k]) === key);
+  return res;
+}, {});
+const updateArray = (arr,dict) => arr.map(entry => dict[entry] || entry);
+
+const newId = (prefix, existing = null) => {
+  if (!existing) existing = [];
+  if (!Array.isArray(existing)) existing = Object.keys(existing);
+  for (let i = 1; i < 100; i++) {
+    if (!existing.includes(prefix+i))
+      return prefix+i;
+  }
+  return null;
+}
+
+const getPlayer = (playerInfo,existingPlayers) => {
+  const playerId = newId(
+    (playerInfo.name ? playerInfo.name.trim().charAt(0).toLowerCase() : 'x') + 'x',
+    existingPlayers
+  );
+  if (!playerInfo.record) playerInfo.record = [0,0,0];
+  return [playerId, playerInfo];
+};
+
+const getPlayers = (newPlayerList, existing) =>
+  newPlayerList.reduce((obj, name) => {
+      const [id,val] = getPlayer({name}, obj);
+      obj[id] = val; return obj;
+    }, {...existing}
+  );
