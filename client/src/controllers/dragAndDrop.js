f5dc40dc49c3081a618809130fb99f5b8f47a8da
bathtaters <nomoremrnice7@gmail.com>
2021-11-04 05:24:12 -0400

    Split and improved drag and drop component

--- client/src/controllers/dragAndDrop.js
+++ client/src/controllers/dragAndDrop.js
@@ -1,6 +1,7 @@
 // Drag & Drop actions
-const isoDateRe = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?\w/;
 
+// Date parsing
+const isoDateRe = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?\w/;
 const parseDates = obj => {
   if (typeof obj === 'object') {
     if (Array.isArray(obj)) return obj.map(parseDates);
@@ -12,22 +13,72 @@ const parseDates = obj => {
   return obj;
 }
 
+// Data extraction helpers
+const getStored = (ev, dataType, ignoreDates=true) => {
+  let data = ev.dataTransfer.getData(dataType);
+  if (!data) return data;
+  data = JSON.parse(data);
+  if (!ignoreDates) data = parseDates(data);
+  return data;
+}
+const getPublicData = (ev, privateDataType) => ev.dataTransfer.types.find(t => !privateDataType || privateDataType !== t);
+const getDataUsing = (getData, args = []) => typeof getData === 'function' ? getData(...args) : getData;
+
+
 // Start Drag
-const start = itemData =>
-  ev => ev.dataTransfer.setData("text/plain", JSON.stringify(itemData));
+const start = (getData, getTestData, dataType='text/json') =>
+  ev => {
+    ev.dataTransfer.effectAllowed = "move";
+    ev.dataTransfer.dropEffect = "move";
+    ev.dataTransfer.setData(
+      dataType,
+      JSON.stringify(getDataUsing(getData, [ev]))
+    );
+    if (getTestData) ev.dataTransfer.setData(
+      typeof getTestData === 'function' ? getTestData(ev) : getTestData,
+      "PUBLIC"
+    );
+  };
 
 // Apply highlight classes while dragging over
-const enter = highlightCls =>
-  ev => highlightCls.forEach(c => ev.target.classList.add(c));
-const leave = highlightCls =>
-  ev => highlightCls.forEach(c => ev.target.classList.remove(c));
+const enter = (highlightCls, illegalCls, canDrop, getData, privateDataType) =>
+  ev => {
+    if (canDrop) {
+      if (!canDrop(
+        ev.dataTransfer.types,
+        getDataUsing(getData, [ev]),
+        getPublicData(ev, privateDataType),
+        ev
+      )) return illegalCls.forEach(c => ev.target.classList.add(c));
+    }
+    highlightCls.forEach(c => ev.target.classList.add(c));
+  };
+
+const leave = (highlightCls, illegalCls, canDrop, getData, privateDataType) =>
+  ev => {
+    if (canDrop) {
+      if (!canDrop(
+        ev.dataTransfer.types,
+        getDataUsing(getData, [ev]),
+        getPublicData(ev, privateDataType),
+        ev
+      )) return illegalCls.forEach(c => ev.target.classList.remove(c));
+    }
+    highlightCls.forEach(c => ev.target.classList.remove(c));
+  };
 
 // End Drag (Highlight Class clears any highlight classes still remaining)
-const drop = (itemData,action,highlightCls=null) => ev => {
-  preventDef(ev);
-  ev.stopPropagation();
-  const itemFrom = parseDates(JSON.parse(ev.dataTransfer.getData("text")));
-  action(itemFrom, itemData);
+const drop = (getData, action, highlightCls=null, canDrop, getCanDropData, dataType='text/json') => ev => {
+  preventDef(ev); ev.stopPropagation();
+
+  if (canDrop && !canDrop(
+    ev.dataTransfer.types,
+    getDataUsing(getCanDropData, [ev]),
+    getPublicData(ev, dataType),
+    ev
+  )) return;
+
+  action(getStored(ev, dataType, true), getDataUsing(getData, [ev,dataType]));
   if (highlightCls) leave(highlightCls)(ev);
   return false;
 };
---
+++ client/src/views/Components/DragBlock.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import dragHandle, { preventDef } from '../../controllers/dragAndDrop';
+
+function DragBlock({
+  storeData, onDrop, children, canDrop, storeTestData, dataType='text/json',
+  className = "", unavailableCls = "", availableCls = "",
+  highlightClsArray = [], illegalClsArray = [], 
+  draggable = true, droppable = true, isAvailable = true,
+}) {
+  const highlightBase = ['border-double','bg-opacity-50','max-border'];
+  const illegalBase = []; //['border-double','bg-opacity-50','max-border', 'neg-border', 'neg-bgd'];
+  const availableBase = "border-opacity-100 border-dashed dimmer-border" +
+    (draggable ? " hover:bg-opacity-50" : "");
+  const highlightCombo = highlightClsArray && highlightClsArray.length ?
+    highlightBase.concat(highlightClsArray) : highlightBase;
+  const illegalCombo = illegalClsArray && illegalClsArray.length ?
+    illegalBase.concat(highlightClsArray) : illegalBase;
+
+  return pug`
+    .border.max-bgd.bg-opacity-0.border-opacity-0(
+      className=(isAvailable ? availableBase : unavailableCls)+" "+className
+      draggable=(isAvailable && draggable)
+      onDragStart=(draggable ? dragHandle.start(storeData, storeTestData, dataType) : null)
+      onDragEnter=dragHandle.enter(highlightCombo, illegalCombo, canDrop, storeTestData, dataType)
+      onDragOver=preventDef
+      onDragLeave=dragHandle.leave(highlightCombo, illegalCombo, canDrop, storeTestData, dataType)
+      onDrop=(droppable ? dragHandle.drop(storeData, onDrop, highlightCombo, canDrop, storeTestData, dataType) : null)
+    )= children
+  `;
+}
+
+DragBlock.propTypes = {
+  storeData: PropTypes.oneOfType([PropTypes.func,PropTypes.object]),
+  onDrop: PropTypes.func,
+  children: PropTypes.node,
+  canDrop: PropTypes.func,
+  storeTestData: PropTypes.oneOfType([PropTypes.func,PropTypes.string]),
+  dataType: PropTypes.string,
+  className: PropTypes.string,
+  highlightClsArray: PropTypes.arrayOf(PropTypes.string),
+  illegalClsArray: PropTypes.arrayOf(PropTypes.string),
+  unavailableCls: PropTypes.string,
+  availableCls: PropTypes.string,
+  draggable: PropTypes.bool,
+  droppable: PropTypes.bool,
+  isAvailable: PropTypes.bool,
+}
+
+export default DragBlock;
