ef6560fe8eaee03b7da7b0c29b7d7cbe2bd3f39e
bathtaters <nomoremrnice7@gmail.com>
2021-11-04 05:48:55 -0400

    centralized user messages

--- client/src/assets/strings.js
+++ client/src/assets/strings.js
@@ -9,20 +9,27 @@ export const defaultDraftTitle = "New Draft";
 
 
 // Messages
-export const createPlayerMsg = name => name + " does not exist. Would you like to create a profile?";
-export const duplicatePlayerMsg = name => name + " is already in this draft.";
-export const unsavedPlayerMsg = name => name + " has not been added to the draft. Would you like to add now?";
-export const deletePlayerMsg = name => "Are you sure you want to delete "+name+"? All of their info will be lost.";
+const defPlayer = 'This player';
+export const createPlayerMsg = name => (name || defPlayer) + " does not exist. Would you like to create a profile?";
+export const duplicatePlayerMsg = name => (name || defPlayer) + " is already in this draft.";
+export const unsavedPlayerMsg = name => (name || defPlayer) + " has not been added to the draft. Would you like to add now?";
+export const deletePlayerMsg = name => "Are you sure you want to delete "+(name || defPlayer.toLowerCase())+"? All of their info will be lost.";
+export const deleteDraftMsg = name => "Are you sure you want to delete "+(name || 'this draft')+"? It will be lost for good.";
 
 
 // --- Lists --- \\
-
+export const statusInfo = [
+  {label: 'N/A', class: "dim-color"},
+  {label: 'Not Started', class: "pos-color"},
+  {label: 'Active', class: "neg-color"},
+  {label: 'Complete', class: "max-color"},
+];
 export const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-
+export const statsHeader = ['Name','W','L','D','%','OMW','OGW'];
 
 // --- Formatting --- \\
 
 export const formatMatchTitle = (matchPlayers, playerData) =>
-  Object.keys(matchPlayers).map(id => playerData[id].name).join(' vs. ');
+  Object.keys(matchPlayers).map(id => (playerData[id] && playerData[id].name) || '?').join(' vs. ');
 
-export const formatRecord = (record, braces=true) => (braces?'[ ':'')+record.join(' - ')+(braces?' ]':'');
+export const formatRecord = (record, braces=true) => (braces?'[ ':'')+(record || ['','']).join(' - ')+(braces?' ]':'');
