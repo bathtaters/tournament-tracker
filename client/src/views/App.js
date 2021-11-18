67eb3d4a5e8f2766464a80e791e864ae474ff3f9
bathtaters <nomoremrnice7@gmail.com>
2021-11-18 02:09:05 -0500

    use api server instead of test data

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import Header from "./Header";
@@ -7,23 +8,45 @@ import Draft from "./Draft";
 import Players from "./Players";
 import Profile from "./Profile";
 
-// import callApi from "../controllers/callApi";
-import testData from "../assets/testData";
+import { setDrafts } from "../models/drafts";
+import { setPlayers } from "../models/players";
+import { setSchedule } from "../models/schedule";
+import getDays from '../controllers/getDays';
+import callApi from "../controllers/callApi";
+// import testData from "../assets/testData";
 
 function App() {
-  const [state, loadData] = useState({ data: {} });
+  const dispatch = useDispatch();
+  const [pageData, setPageData] = useState({ loaded: false });
+  const [pageSettings, setSettings] = useState(null);
 
-  // useEffect(() => callApi('test_backend').then(loadData), []);
-  useEffect(() => loadData({loaded:true, data:testData}), []);
+  const setAll = useCallback(({ drafts, players, schedule, settings, err }) => {
+    if (err) return setPageData({ loaded: true, err });
+    dispatch(setDrafts(drafts));
+    dispatch(setPlayers(players));
+    dispatch(setSchedule(getDays(settings.dateRange, schedule)));
+    // setSettings({ drafts, players, schedule, settings, err });
+    setSettings(settings);
+    setPageData({ loaded: true });
+  }, [dispatch]);
+
+  useEffect(() => callApi('get/all').then(setAll), [setAll]);
+  // useEffect(() => loadData({loaded:true, data:testData}), []);
+
+  const resetData = e => { e.target.disabled = true; callApi('set/resetDb').then(() => e.target.disabled = false); };
+  // const resetData = e => { e.target.disabled = true; setTimeout(t => t.disabled = false, 3000, e.target); };
   
-  const loggedIn = true;
   return pug`
     .min-h-screen.relative
+      // .p-4
+        p.m-2= JSON.stringify(pageSettings && Object.keys(pageSettings))
+        p.m-2= JSON.stringify(pageSettings)
+
       Router
-        Header(title=((state.data.settings && state.data.settings.title) || "Tournament Tracker"))
+        Header(title=((pageSettings && pageSettings.title) || "Tournament Tracker"))
         
         .m-2
-          if state.loaded && !state.err && loggedIn
+          if pageData.loaded && !pageData.err
             Switch
               Redirect(exact=true from="/" to="/home")
 
@@ -42,15 +65,14 @@ function App() {
               Route(path="*")
                 .text-center This is not a page.
 
-          else if !state.loaded
+            .block.text-center.m-6
+              input(type="button" value="Reset Data" onClick=resetData)
+
+          else if !pageData.loaded
             h4.text-center Loading...
 
-          else if state.err
-            h4.text-center= 'Error: '+state.err
-          
           else
-            h4.text-center
-              a(href="#") Login Here
+            h4.text-center= 'Error: '+(pageData.err || 'N/A')
   `;
 }
 
