c5d3ca6d8fd62a780b0cbb4105756ea23fdb1264
bathtaters <nomoremrnice7@gmail.com>
2021-12-04 00:29:06 -0500

    updated date format

--- client/src/controllers/getDays.js
+++ client/src/controllers/getDays.js
@@ -2,7 +2,10 @@
 // Eventually move to API
 
 // One day in ms
-const oneDay = 24*60*60*1000;
+export const oneDay = 24*60*60*1000;
+
+// Get Date string
+export const toDate = dt => dt && dt.toISOString().slice(0,10);
 
 // Compare dates (ignoring time)
 export const sameDay = (dateA, dateB = (new Date()).getTime()) => {
@@ -12,26 +15,11 @@ export const sameDay = (dateA, dateB = (new Date()).getTime()) => {
 }
 
 // Build array of days from start/end dates
-const dayArray = (start, end) => {
-  if (end < start) return [];
-  let arr = []; const e = end;
-  for (let d = start; d < e; d += oneDay) {
-    arr.push(d);
+export default function getDays(start, end) {
+  let arr = ['none'];
+  if (end < start) return arr;
+  for (let d = start; d <= end; d += oneDay) {
+    arr.push(toDate(new Date(d)));
   }
   return arr;
-}
-
-// Insert schedule info into day array
-const mapDates = schedule => day => {
-  let dayta = schedule.find(d => sameDay(d.day, day));
-  if (dayta) { dayta = JSON.parse(JSON.stringify(dayta)); }
-  return dayta || {day};
-}
-
-// Convert range limits & schedule data into ordered array
-export default function getDays(range, scheduleData) {
-  let days = dayArray(...range);
-  days = days.map(mapDates(scheduleData));
-  days.unshift(scheduleData.find(d => !d.day) || {});
-  return days;
 }
