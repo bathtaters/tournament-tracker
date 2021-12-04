673fda5fff26f043e1f910e3630c6b103dc88365
bathtaters <nomoremrnice7@gmail.com>
2021-12-04 00:17:04 -0500

    config for rtk query

--- api/server.js
+++ api/server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9090;
 
+// Setup middleware
+app.use(express.json());
+
 // Logging
 app.use(function(req, res, next) {
     console.log('REQ:',req.originalUrl,req.method,req.body);
     res.sendAndLog = (...args) => { console.log('RES:', ...args.map(a => typeof a === 'object' ? Object.keys(a) : a)); return res.send(...args); };
+    res.setHeader('Access-Control-Allow-Origin','*'); // To bypass CORS
     return next();
 });
 
 // Setup routes
 app.use('/api/v1', require('./routes/base'));
 app.use('/api/v1/draft', require('./routes/draft'));
+app.use('/api/v1/match', require('./routes/match'));
 app.use('/api/v1/player', require('./routes/player'));
 
 // Error handler
