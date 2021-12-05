b5e810f69a828e9b2c0f80e9422ba96e93acecee
bathtaters <nomoremrnice7@gmail.com>
2021-12-04 23:00:32 -0500

    split controllers from views

--- client/src/controllers/records.js
+++
@@ -1,5 +0,0 @@
-// Dealing with Player records
-
-export const recordToPoints = record => record[0] * 3 + (record[2] || 0) * 1;
-
-export const sortByRecord = players => Object.keys(players).sort((a,b) => recordToPoints(b) - recordToPoints(a));
