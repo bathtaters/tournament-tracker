17d3ef3512859026da1fc1d614e1045094a76047
bathtaters <nomoremrnice7@gmail.com>
2021-12-15 03:04:25 -0500

    fixed optimistic updates and tags

--- client/src/controllers/misc.js
+++ client/src/controllers/misc.js
@@ -7,9 +7,30 @@ export const findObj = (obj, value, innerkey = null) => {
 }
 
 // Checks that 2 arrays are equal (Must be 1D arrays, 2 falsy vars will also be equal)
-
 export const equalArrays = (a,b) =>
   (!a && !b) || (a && b && 
     a.length === b.length && 
     a.every((v,i) => b[i] === v)
   );
+
+// Generates a temporary ID
+const tempId = type => n => `TEMPID-${type}-${('0000'+n).slice(-4)}`;
+export const nextTempId = (type, exists) => {
+  if (!exists) return tempId(type)(0);
+  let n = 0, id; const getId = tempId(type);
+  while (++n < 10000) { 
+    id = getId(n);
+    if (!exists.includes(id)) break;
+  } return id;
+}
+
+// Build a playerId list from rank (Include missing players if listAll=true)
+export const getPlayerList = (ranking, players, listAll=false) => {
+  if (!players) return ranking || [];
+  let list = [];
+  if (ranking) {
+    list = ranking.filter(p => players[p]);// && !players[p].isteam);
+    if (listAll) list.push(...Object.keys(players).filter(p => !ranking.includes(p)));
+  } else if (listAll) list.push(...Object.keys(players));
+  return list;
+}
--- client/src/models/playerApi.js
+++ client/src/models/playerApi.js
@@ -1,28 +1,51 @@
-import { baseApi } from './baseApi';
+import { baseApi, tagIds } from './baseApi';
+import { draftApi } from './draftApi';
+import { nextTempId } from '../controllers/misc';
 
-const playerApi = baseApi.injectEndpoints({
+
+export const playerApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     // Query
     player:  build.query({
       query: (id=null) => `player/${id || 'all'}`,
-      providesTags: ['Player'],
+      transformResponse: res => console.log('PLAYER',res) || res,
+      providesTags: tagIds('Player'),
+    }),
+    playerDrafts:  build.query({
+      query: (id) => `player/${id}/drafts`,
+      transformResponse: res => console.log('PLAYER_DET',res) || res,
+      providesTags: tagIds('PlayerDetail',{ all: false }),
     }),
 
     // Mutations
     createPlayer: build.mutation({
-      query: ({ name }) => ({ url: `player`, method: 'POST', body: { name }, }),
-      invalidatesTags: ['Player'],
+      query: (body) => ({ url: `player`, method: 'POST', body, }),
+      transformResponse: res => console.log('ADD_PLAYER',res) || res,
+      invalidatesTags: tagIds('Player', { addAll:['Breakers'] }),
+      onQueryStarted(body, { dispatch, getState }) {
+        const breakers = getState().dbApi.queries['breakers(undefined)'];
+        const id = nextTempId('PLAYER', breakers && breakers.data && breakers.data.ranking);
+        dispatch(playerApi.util.updateQueryData('player', undefined, draft => { draft[id] = body; }));
+        dispatch(draftApi.util.updateQueryData('breakers', undefined, draft => { draft.ranking.push(id); }));
+      },
     }),
     deletePlayer: build.mutation({
       query: id => ({ url: `player/${id}`, method: 'DELETE' }),
-      invalidatesTags: ['Player'],
+      transformResponse: res => console.log('DEL_PLAYER',res) || res,
+      invalidatesTags: tagIds('Player', { addAll:['Breakers'] }),
       onQueryStarted(id, { dispatch }) {
         dispatch(playerApi.util.updateQueryData('player', undefined, draft => { delete draft[id]; }));
+        dispatch(draftApi.util.updateQueryData('breakers', undefined, draft => {
+          const idx = draft.ranking ? draft.ranking.indexOf(id) : -1;
+          if (idx > -1) draft.ranking.splice(idx,1);
+          delete draft[id];
+        }));
       },
     }),
     updatePlayer: build.mutation({
       query: ({ id, ...body }) => ({ url: `player/${id}`, method: 'PATCH', body }),
-      invalidatesTags: ['Player'],
+      transformResponse: res => console.log('UPD_PLAYER',res) || res,
+      invalidatesTags: tagIds('Player'),
       onQueryStarted({ id, ...body }, { dispatch }) {
         dispatch(playerApi.util.updateQueryData('player', undefined, draft => { 
           Object.assign(draft[id], body); 
@@ -33,9 +56,10 @@ const playerApi = baseApi.injectEndpoints({
       },
     }),
   }),
+  overrideExisting: true
 });
 
 export const {
-  usePlayerQuery, useCreatePlayerMutation,
+  usePlayerQuery, usePlayerDraftsQuery, useCreatePlayerMutation,
   useDeletePlayerMutation, useUpdatePlayerMutation,
 } = playerApi;
