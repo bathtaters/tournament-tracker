2ad861873eef399e90317ea474f4b0db6fc2197b
bathtaters <nomoremrnice7@gmail.com>
2022-01-15 04:52:28 -0500

    setup remote dev tools

--- client/src/controllers/store.js
+++ client/src/controllers/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
+import devToolsEnhancer from 'remote-redux-devtools';
 import { baseApi } from '../models/baseApi'
 
 export default configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
   },
+  devTools: false,
+  enhancers: [devToolsEnhancer({
+    name: 'lol-retreat',
+    hostname: '192.168.0.179',
+    port: 8000,
+    realtime: true, 
+  })],
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
 })
