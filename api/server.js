16df9e47b3fc830fd713d92021f6461e19514fc5
bathtaters <nomoremrnice7@gmail.com>
2022-01-15 13:32:41 -0500

    update error response

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // Logging
 app.use(function(req, res, next) {
     console.log('REQ:',req.originalUrl,req.method,req.body);
-    res.sendAndLog = (...args) => { console.log('RES:', ...args.map(a => typeof a === 'object' ? Object.keys(a) : a)); return res.send(...args); };
+    res.sendAndLog = (...args) => { console.log('RES:', ...args.map(a => typeof a === 'object' && !a.error ? Object.keys(a) : a)); return res.send(...args); };
     res.setHeader('Access-Control-Allow-Origin','*'); // To bypass CORS
     return next();
 });
@@ -20,13 +20,11 @@ app.use('/api/v1/match', require('./routes/match'));
 app.use('/api/v1/player', require('./routes/player'));
 
 // Error handler
+const defaultError = { status: 500, message: 'Unknown server error' };
 app.use(function(err, req, res, next) {
     if (!req.error) req.error = err;
-    res.status((req.error && req.error.status) || 500);
-    return res.sendAndLog((req.error && req.error.message) || { message: 'Unknown server error' });
+    res.status((req.error && req.error.status) || defaultError.status);
+    return res.sendAndLog({ error: req.error ? req.error.message || req.error : defaultError.message });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Testing
-// require('./testing/checkDb');
