725cff55505c48575d3e3c6e2ae163cf5daabf4c
bathtaters <nomoremrnice7@gmail.com>
2022-01-16 15:04:04 -0500

    move instructions to separate readme

--- api/db/admin/getConnStr.js
+++ api/db/admin/getConnStr.js
@@ -1,34 +1,12 @@
 //--- Get URI to connect to DB ---//
 
-/* SAMPLE dbServerCfg.json
-// NOTE: This file should placed in this directory w/ below data
-// Connection string will substitute %<VAR> for users[n].<var> or server.<var>
-// It will also substitute $<envVars> based on envVars (below)
-{
-  "connectionString": "%PROTO://%NAME:%PASS@%DOM:%PORT/%DB?sslmode=%MODE&sslrootcert=%CERT&options=%OPTS",
-  "users": [
-    { "name": "dbUser", "pass": "password123" },
-    { "name": "api",    "pass": "password456" },
-  ],
-  "server": {
-    "proto": "postgresql",
-    "dom": "free-tier.gcp-us-central1.cockroachlabs.cloud",
-    "port": "26257",
-    "db": "defaultdb",
-    "mode": "verify-full",
-    "cert": "$HOME/.postgresql/root.crt",
-    "opts": ""
-  }
-}
-//*/
-
 // Environment variables to substitute (Uses $ prefix)
 /* istanbul ignore next */
 const envVars = { "HOME": process.env.HOME || "", "env:appdata": process.env.APPDATA || "" };
 
 // Import
 const utils = require('../../services/utils');
-const serverCfg = require('./dbServerCfg.json'); // Config data
+const serverCfg = require('../../config/dbServer.json');
 
 // Convert credentials to connectionString
 module.exports = (dbUser, config = null) => {
