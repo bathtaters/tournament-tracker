875782168cb9c9b02efaf431dcb19779907e27c8
bathtaters <nomoremrnice7@gmail.com>
2022-01-16 18:40:36 -0500

    automate versioning and catch 404s

--- api/src/server.js
+++ api/src/server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9090;
+const version = (require('../package.json').version || '1').split('.',1)[0];
 
 // Setup middleware
 app.use(express.json());
 app.use(require('./middleware/log.middleware'));
 
 // Setup routes
-app.use('/api/v1', require('./routes/base.routes'));
-app.use('/api/v1/draft', require('./routes/draft.routes'));
-app.use('/api/v1/match', require('./routes/match.routes'));
-app.use('/api/v1/player', require('./routes/player.routes'));
+app.use('/api/version', (_, res) => res.sendAndLog('v' + version));
+app.use(`/api/v${version}`,        require('./routes/base.routes'));
+app.use(`/api/v${version}/draft`,  require('./routes/draft.routes'));
+app.use(`/api/v${version}/match`,  require('./routes/match.routes'));
+app.use(`/api/v${version}/player`, require('./routes/player.routes'));
 
-// Error handler
+// Error handling
+app.use(() => { throw require('./config/constants.json').missingError; })
 app.use(require('./middleware/error.middleware'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`lol-retreat api (v${version}) started. Listening on port ${port}.`));
