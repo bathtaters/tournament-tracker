334916e523160cd250348932408d35898a356f4e
bathtaters <nomoremrnice7@gmail.com>
2022-01-17 02:16:33 -0500

    clean out redundancies w/ connect & trim

--- api/src/db/admin/directOps.js
+++ api/src/db/admin/directOps.js
@@ -1,40 +1,25 @@
 /* *** ADVANCED SQL UI *** */
 const fs = require("fs/promises");
-const join = require("path").join;
 const db = require('./connect');
-const utils = require('../../utils/sqlUtils');
-
-// DB Parameters
-const resetDbFiles = [ join(__dirname,'resetDb.sql') ];
-const attemptsDefault = 15;
-const retriesDefault = 0;
-
-// Force init on load
-if (!db.isConnected()) db.openConnection();
-
-// Base DB controller
-function operation(clientOp = client => null, maxAttempts = attemptsDefault, retryCount = retriesDefault) {
-    return db.runOperation(clientOp, maxAttempts, retryCount);
-}
+const logger = console;
 
 // Execute query
-function query(text, args = [], splitArgs = null, maxAttempts = attemptsDefault, retryCount = retriesDefault) {
+function query(text, args = [], splitArgs = null) {
     // Split text/args into arrays
     if (!Array.isArray(text)) text = text.split(';').filter(q=>q.trim());
     text = text.map(q => /;\s*$/.test(q) ? q : q+';');
     
     // logger.debug('QUERIES:',text);
-    return operation(
+    return db.runOperation(
         // client => utils.awaitEach(client.query, text.map((q,i) => [q, splitArgs ? args[i] : args])),
-        client => Promise.all(text.map((q,i) => client.query(q, splitArgs ? args[i] : args))),
-        maxAttempts, retryCount
+        client => Promise.all(text.map((q,i) => client.query(q, splitArgs ? args[i] : args)))
     );
 }
 
 // Load commands from a .SQL file
 async function loadFiles(pathArray) {
     // Read files into array
-    let sqlFileText = await Promise.all(pathArray.map(path =>
+    let sqlFileText = await Promise.all(pathArray.filter(Boolean).map(path =>
         fs.readFile(path)
         .then(file => file.toString().split(';'))
         .catch(e => { throw new Error(`Unable to read SQL file '${path}': ${e.message || e.description || e}`) })
@@ -51,17 +36,10 @@ async function loadFiles(pathArray) {
     return sqlFileText;
 }
 
-// Load and execute file
-const execFiles = (pathArray, maxAttempts = attemptsDefault, retryCount = retriesDefault) => 
-    loadFiles(pathArray).then(text => query(text,[],maxAttempts,retryCount));
-
-// Completely erase the database & create a new one
-const resetDb = (maxAttempts = attemptsDefault, retryCount = retriesDefault) =>
-    execFiles(resetDbFiles, maxAttempts, retryCount);
-
 module.exports = {
-    operation, query, execFiles, resetDb,
+    query, loadFiles,
+    operation: db.runOperation,
     init: db.openConnection,
     deinit: db.closeConnection,
-    loadFiles
+    execFiles: pathArray => loadFiles(pathArray).then(text => query(text,[]))
 }
