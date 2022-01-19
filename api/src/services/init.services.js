b62abae6c14def11504781cf1c36f9ddc97c7d2e
bathtaters <nomoremrnice7@gmail.com>
2022-01-19 04:48:16 -0500

    rename service

--- api/src/services/init.services.js
+++ api/src/services/init.services.js
@@ -2,10 +2,10 @@ const logger = console;
 const db = require('../db/admin/connect');
 const utils = require('../utils/sqlUtils');
 
-async function initServer() {
+async function initServices() {
   utils.initNumberParsing(); // Setup integer parsing
   await db.openConnection(); // Connect to DB
   logger.log('Background services started.');
 }
 
-module.exports = initServer;
+module.exports = initServices;
