7e253e67ec3c1894757c8c6e40460dd453d17e15
bathtaters <nomoremrnice7@gmail.com>
2022-01-21 17:07:18 -0500

    move doc into readme & update swap route

--- api/src/routes/match.routes.js
+++ api/src/routes/match.routes.js
@@ -1,31 +1,11 @@
-` *** Public API commands ***
-
- -- Get Matches --
- GET: ./match/{all | <matchId>}
- Returns: { ...data from all/a match }
-
- GET: ./match/all/draft/<draftId>
- Returns: { ...data from all matches w/in draft }
-
- -- Edit Matches --
-POST: ./match/<matchId> { ...reportData }
-Returns: Report wins/losses/etc for match => { [draft]id }
-
-DELETE: ./match/<matchId>
-Returns: Clear reported wins/losses/etc for match => { [draft]id }
-
-PATCH: ./match/<matchId>
-Returns: Update reported wins/losses/etc for match => { [draft]id }
-
-PATCH: ./match/swap [ { id: <playerId>, match: <matchId> }, ... ]
-Returns: Swap two players w/in matches => { [draft]id }
-`
-
 // Init
 const router = require('express').Router();
 const catcher = require('../middleware/catch.middleware');
 const controller = require('../controllers/match.controllers');
 const action = require('../controllers/action.controllers');
+
+
+// *** Match API commands *** \\
 
 // Gets
 router.get('/all/draft/:draftId', catcher(controller.getDraftMatches));
@@ -33,7 +13,7 @@ router.get('/all', catcher(controller.getAllMatches));
 router.get('/:matchId', catcher(controller.getMatch));
 
 // Sets
-router.patch('/util/swap', catcher(action.swapPlayers));
+router.post( '/swap', catcher(action.swapPlayers));
 router.post(  '/:id', catcher(controller.reportMatch));
 router.delete('/:id', catcher(controller.unreportMatch));
 router.patch( '/:id', catcher(controller.updateMatch));
