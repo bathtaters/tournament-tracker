29d3753a0bc778cc912a3e105f209ece3b05e176
bathtaters <nomoremrnice7@gmail.com>
2022-01-21 20:03:31 -0500

    fix infinite loop in unflat w/ elemSize < 1

--- api/src/services/matchGenerators/matchGen.utils.js
+++ api/src/services/matchGenerators/matchGen.utils.js
@@ -8,6 +8,7 @@ exports.anyElements = (arrA, arrB) => arrA.some(elem => arrB.includes(elem));
 
 // "Un-flatten" array, group every <elemSize> entries
 exports.unflat = (array, elemSize) => {
+  if (elemSize < 1) return array.slice();
   let newArr = [];
   for(let i=0; i < array.length; i += elemSize) {
     newArr.push(array.slice(i, i + elemSize));
@@ -19,13 +20,13 @@ exports.unflat = (array, elemSize) => {
 // Reverse-search 2D Array for a replacement
 // (loops around when it reaches the start of the array)
 // Callback: (value, inner array, inner array idx) => true/false
-exports.revReplace2dIndex = (arr2d, startIdx, replaceCb) => {
+exports.revReplace2dIndex = (arr2d, startIdx, isReplace = () => {}) => {
   const start = (startIdx || arr2d.length) - 1; // ignores itself
   const end = startIdx % arr2d.length; // ensure end is in arr2d
   
   for (let i = start; i !== end; i = (i || arr2d.length) - 1) {
     for (let j = arr2d[i].length; j-- > 0; ) {
-      if (replaceCb(arr2d[i][j], arr2d[i], j)) return [i,j];
+      if (isReplace(arr2d[i][j], arr2d[i], j)) return [i,j];
     }
   }
 };
