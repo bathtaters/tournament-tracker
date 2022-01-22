a355a49fbbc1192c034e36d0034284b4718c3e45
bathtaters <nomoremrnice7@gmail.com>
2022-01-21 19:56:50 -0500

    change reserved var name 'package' to pkg

--- api/src/config/meta.js
+++ api/src/config/meta.js
@@ -1,13 +1,13 @@
 // Load project metadata
 
 const join = require('path').join;
-const package = require('../../package.json');
+const pkg = require('../../package.json');
 
 module.exports = {
-  name: package.name || 'Untitled',
-  version: package.version || '0',
-  port: process.env.PORT || +package.config.port || 9090,
-  apiVersion: (package.version || '1').split('.',1)[0],
+  name: pkg.name || 'Untitled',
+  version: pkg.version || '0',
+  port: process.env.PORT || +pkg.config.port || 9090,
+  apiVersion: (pkg.version || '1').split('.',1)[0],
   rootPath: join(__dirname,'..','..'), // Update if this file moves
   sqlFilesPath: join(__dirname,'..','db','sql'), // Update if this file moves
 }
