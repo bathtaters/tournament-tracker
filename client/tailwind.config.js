82955aadbb9edddfe945a62e175e7040ba73fad0
bathtaters <nomoremrnice7@gmail.com>
2022-01-25 17:32:53 -0500

    set for JS projects only

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 // const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.js', './public/index.html'],
   theme: {
     extend: {
       // colors: { green: colors.emerald, },
