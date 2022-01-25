1c68adf1a109ace3b663fd7f64afc6535fc4b21e
bathtaters <nomoremrnice7@gmail.com>
2022-01-25 17:41:57 -0500

    add object filtering func

--- api/src/utils/shared.utils.js
+++ api/src/utils/shared.utils.js
@@ -1,21 +1,20 @@
 // -- Common Utility Functions -- //
 const logger = console;
 
-// Convert array of objects into single object using 'key' field as keys
-// (Used to index pg return values by ID)
-/**
- * Convert array of objects into a result object using 'key' field in each entry as that entry's key
- * @param {string} key - Key of array entry property to use as unique key in result object
- * @param {Object} options - Additional options for conversion
- * @param {boolean} [options.delKey=true] - Truthy will delete entry['key'] after assigning it to result object['key']
- * @param {string} [options.valKey] - If entered will use entry['valKey'] instead of entry in result object 
- * @returns {convertArray} function to convert input array into result object
- */
+
 /**
  * Converts object array into result object using input from arrToObj
  * @callback convertArray 
  * @param {Object[]} objArray - Array of objects to be converted
  * @returns {Object} Result object
+ */
+/**
+ * Convert array of objects into a result object using 'key' field in each entry as that entry's key
+ * @param {String} key - Key of array entry property to use as unique key in result object
+ * @param {Object} options - Additional options for conversion
+ * @param {Boolean} [options.delKey=true] - Truthy will delete entry['key'] after assigning it to result object['key']
+ * @param {String} [options.valKey] - If entered will use entry['valKey'] instead of entry in result object 
+ * @returns {convertArray:Object} function to convert input array into result object
  */
 exports.arrToObj = (key, { delKey=true, valKey=null }={}) =>
   obj => typeof obj !== 'object' ? 
@@ -30,21 +29,22 @@ exports.arrToObj = (key, { delKey=true, valKey=null }={}) =>
     }, {});
 
 
+
+/**
+ * Tests if 'entry' should be before 'item' in array.
+ * @callback isBefore 
+ * @param {*} entry - Array entry being tested
+ * @param {*} item - Item to be inserted
+ * @returns {Boolean} If 'entry' is ordered before 'item'
+ */
 /**
  * Place 'item' into 'array' in order.
  * This mutates the array & returns it.
  * Based off 'sortedIndex' by 'Web_Designer' on Stack Exchange
  * @param {Array} array - Array to insert into
  * @param {*} item - Item to insert in array
- * @param {isBefore:boolean} [isBefore=((a,b)=>a<b)] - Callback to test order of items
+ * @param {isBefore:Boolean} [isBefore=((a,b)=>a<b)] - Callback to test order of items
  * @returns {Array} 'array' with 'item' (Modifys original 'array' also)
- */
-/**
- * Tests if 'entry' should be before 'item' in array.
- * @callback isBefore 
- * @param {*} entry - Array entry being tested
- * @param {*} item - Item to be inserted
- * @returns {boolean} If 'entry' is ordered before 'item'
  */
 exports.insertSorted = (array, item, isBefore = (entry,item) => entry < item) => {
   // Get index
@@ -57,4 +57,18 @@ exports.insertSorted = (array, item, isBefore = (entry,item) => entry < item) =>
   // Insert item
   array.splice(low, 0, item);
   return array;
+};
+
+/**
+ * Create a shallow copy of 'object' without keys in 'ignoring'
+ * @param {Object} object - Object to copy
+ * @param {String[]} [ignoring=id] - Keys to ignore when copying
+ * @returns {Object} Copied Object
+ */
+exports.filtering = (object, ignoring = ['id']) => {
+  const copy = {};
+  Object.keys(object).forEach(key => {
+    if (!ignoring.includes(key)) copy[key] = object[key];
+  });
+  return copy;
 };
