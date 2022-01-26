0c1849d2d536a9bf5c6bf7477cb693e003e84ce7
bathtaters <nomoremrnice7@gmail.com>
2022-01-26 02:36:01 -0500

    convert json to js for intellisense

---
+++ api/src/config/constants.js
@@ -0,0 +1,30 @@
+module.exports = {
+  defaultError: { 
+    status: 500,
+    message: "Unknown server error"
+  },
+  missingError: {
+    status: 404,
+    message: "Invalid address and/or method."
+  },
+  testError: {
+    status: 418,
+    message: "API is not a Teapot."
+  },
+  clockStates: [
+    "Ended",
+    "Running",
+    "Off",
+    "Paused"
+  ],
+  points: {
+    win: 3,
+    draw: 1,
+    floor: 0.33
+  },
+  recordFields: [
+    "Wins",
+    "Losses",
+    "Draws"
+  ]
+};
