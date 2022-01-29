720c7e50d22d32a118ad6102c6b2d52116b3aa86
bathtaters <nomoremrnice7@gmail.com>
2022-01-29 02:21:40 -0500

    reorganize and add export validation to client

--- api/src/services/init.services.js
+++ api/src/services/init.services.js
@@ -1,24 +1,26 @@
+// Imports
 const logger = require('../utils/log.adapter');
+const exportToClient = require('../config/exportToClient');
+const validation = require('../config/validation');
+const { types } = require("pg");
 const db = require('../db/admin/connect');
 
+// To be run when server starts
 async function initServices() {
-  initNumberParsing(); // Setup integer parsing
-  await db.openConnection(); // Connect to DB
-  logger.log('Background services started.');
-}
+  // Copy validation params to client
+  exportToClient(validation, 'src', 'assets', 'validation.json')
+    .then(p => console.log('Validate config copied to client --  '+p));
 
-module.exports = initServices;
-
-// Init helper functions //
-const { types } = require("pg");
-
-// Setup number parsing for PG module
-const initNumberParsing = () => {
-  // Setup returning ints/floats as numbers
+  // Setup integer parsing for pg
   types.setTypeParser(types.builtins.INT2, parseInt);
   types.setTypeParser(types.builtins.INT4, parseInt);
   types.setTypeParser(types.builtins.INT8, BigInt);
   // types.setTypeParser(types.builtins.FLOAT4, parseFloat);
   // types.setTypeParser(types.builtins.FLOAT8, parseFloat);
   types.setTypeParser(types.builtins.NUMERIC, parseFloat);
+
+  await db.openConnection(); // Connect to DB
+  logger.log('Background services started.');
 }
+
+module.exports = initServices;
