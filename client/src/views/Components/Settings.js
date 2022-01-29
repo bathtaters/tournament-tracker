82fd2c7d6f5473f8ff98bb87a263d2ca2a449dd6
bathtaters <nomoremrnice7@gmail.com>
2022-01-29 01:39:21 -0500

    hide advanced settings & improve menu layout

--- client/src/views/Components/Settings.js
+++ client/src/views/Components/Settings.js
@@ -6,6 +6,7 @@ import RawData from "./RawData";
 
 import { useSettingsQuery, useUpdateSettingsMutation, useResetDbMutation } from "../../models/baseApi";
 
+import { deepFilter } from "../../controllers/misc";
 import {
   defaultTournamentTitle,
   formatQueryError,
@@ -14,16 +15,24 @@ import {
 
 const settingsRows = [
   { label: 'Title', id: 'title', type: 'text', defaultValue: '',
-    className: "text-base sm:text-xl font-medium m-2",
+    className: "text-base sm:text-xl font-medium m-2 flex w-full items-baseline",
+    inputClass: "max-color pt-1 px-2 w-full",
     transform: (title,data) => title.trim() ? title.trim() : (data && data.title) || defaultTournamentTitle
   },
-  [{ label: 'Start Date', id: 'datestart', type: 'date' },
-  { label: 'End Date', id: 'dateend', type: 'date' }],
-  { label: 'Daily Slots', id: 'dayslots', type: 'number' },
-  [[{ label: 'Auto-Report Byes', id: 'autobyes', type: 'checkbox' },
-  { label: 'Incomplete Events in Player Stats', id: 'includeincomplete', type: 'checkbox' },],
-  [{ label: 'Show Advanced', id: 'showadvanced', type: 'checkbox' },
-  { label: 'Show Raw Data', id: 'showrawjson', type: 'checkbox' },],]
+  [
+    { label: 'Start Date', id: 'datestart', type: 'date' },
+    { label: 'End Date', id: 'dateend', type: 'date' }
+  ],
+  { label: 'Daily Slots', id: 'dayslots', type: 'number', advanced: true },
+  [
+    [
+      { label: 'Show Advanced', id: 'showadvanced', type: 'checkbox' },
+      { label: 'Show Raw Data', id: 'showrawjson', type: 'checkbox', advanced: true },
+    ],[
+      { label: 'Auto-Report Byes', id: 'autobyes', type: 'checkbox', advanced: true },
+      { label: 'Incomplete Events in Player Stats', id: 'includeincomplete', type: 'checkbox', advanced: true },
+    ],
+  ]
 ];
 
 
@@ -52,7 +61,7 @@ function Settings({ hideModal, lockModal }) {
     <div>
       <h3 className="font-light max-color text-center mb-2">Settings</h3>
       <InputForm
-        rows={settingsRows}
+        rows={data.showadvanced ? settingsRows : deepFilter(settingsRows, s => !s.advanced)}
         data={data}
         onSubmit={submitSettings}
         onEdit={lockModal}
