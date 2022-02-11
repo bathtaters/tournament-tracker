544868d2c9a5fd10d9a0535524ee6fdf069ebaf9
bathtaters <nomoremrnice7@gmail.com>
2022-02-10 20:23:42 -0500

    fix default altMsg

--- client/src/pages/common/Loading.js
+++ client/src/pages/common/Loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { formatQueryError } from "../../assets/strings";
 
-function Loading({ loading = false, error = null, altMsg = 'Missing', className = '', TagName = 'div' }) {
+function Loading({ loading = false, error = null, altMsg = 'Missing data', className = '', TagName = 'div' }) {
   return (<TagName className={"italic text-center font-thin "+className}>{
       loading ? 'Loading...' :
       !error ? altMsg :
