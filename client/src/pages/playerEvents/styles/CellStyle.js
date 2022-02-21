0c56526290df876f1dda1c1744a47a223423af8f
bathtaters <nomoremrnice7@gmail.com>
2022-02-20 23:08:36 -0500

    combine similar styles

--- client/src/pages/playerEvents/styles/CellStyle.js
+++ client/src/pages/playerEvents/styles/CellStyle.js
@@ -3,18 +3,14 @@ import React from "react";
 import { scheduleGridClass } from "./PlayerEventStyles";
 const scheduleGridSpan = scheduleGridClass.replace('grid-cols','col-span');
 
-export function HeaderStyle({ span, children }) {
-  return (<h4 className={span ? 'col-span-'+span : ''}>{children}</h4>);
+export function HeaderStyle({ span, className = "", children }) {
+  return (<h4 className={`${className} ${span ? 'col-span-'+span : ''}`}>{children}</h4>);
+}
+
+export function CellStyle({ span, className = "", children }) {
+  return (<HeaderStyle span={span} className={'font-thin base-color ' + className}>{children}</HeaderStyle>);
 }
 
 export function NoEventsStyle() {
   return (<div className={"dim-color italic font-thin text-center my-2 "+scheduleGridSpan}>– None –</div>);
-}
-
-export function CellStyle({ span, className = "", children, ...props }) {
-  return (
-    <h4 className={'font-thin base-color ' + className + (span ? ' col-span-'+span : '')} {...{props}}>
-        {children}
-    </h4>
-  );
 }
