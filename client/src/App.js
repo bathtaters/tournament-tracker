5f14c0dababa79ee5c4f6825991a4d305695ba6e
bathtaters <nomoremrnice7@gmail.com>
2022-02-22 00:39:32 -0500

    split off loading/error and styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,24 @@ import { BrowserRouter } from "react-router-dom";
 
 import Header from "./pages/header/Header";
 import Routing from "./Routing";
-import RawData from "./pages/common/RawData";
+import Loading from "./pages/common/Loading";
+import { AppWrapperStyle, PageWrapperStyle } from "./pages/common/styles/CommonStyles";
 
 import { useSettingsQuery } from "./pages/common/common.fetch";
-
-import { formatQueryError } from "./assets/strings";
 
 function App() {
   const { data, isLoading, error } = useSettingsQuery();
 
+  if (isLoading || error)
+    return <Loading altMsg="Loading your data..." error={error} className="m-8 text-xl" />;
+
   return (
-    <div className="min-h-screen relative">
-      { isLoading ? 
-        <h4 className="m-2 text-center">Loading your data...</h4>
-      : error ? 
-        <h4 className="m-2 text-center">{formatQueryError(error)}</h4>
-      : 
-        <BrowserRouter>
-          <Header title={data && data.title} />
-          <div className="m-2">
-            <Routing />
-            <RawData className="text-xs" data={data} />
-          </div>
-        </BrowserRouter>
-      }
-    </div>
+    <AppWrapperStyle>
+      <BrowserRouter>
+        <Header title={data && data.title} />
+        <PageWrapperStyle><Routing /></PageWrapperStyle>
+      </BrowserRouter>
+    </AppWrapperStyle>
   );
 }
 
--- client/src/pages/common/styles/CommonStyles.js
+++ client/src/pages/common/styles/CommonStyles.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+export function AppWrapperStyle({ children }) {
+  return (<div className="min-h-screen relative">{children}</div>);
+}
+
+export function PageWrapperStyle({ children }) {
+  return (<div className="m-2">{children}</div>);
+}
+
 export function PageTitleStyle({ className = "", children }) {
   return <h2 className={`text-center font-thin ${className}`}>{children}</h2>
 }
