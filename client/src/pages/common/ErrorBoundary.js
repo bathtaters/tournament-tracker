4b28c6e8ad085724036e37a9c01be160ac1ec246
bathtaters <nomoremrnice7@gmail.com>
2022-03-01 01:55:07 -0500

    add listener for async errors

--- client/src/pages/common/ErrorBoundary.js
+++ client/src/pages/common/ErrorBoundary.js
@@ -5,10 +5,29 @@ class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { error: null, errorInfo: null };
+
+    // Catch Async errors
+    this.asyncHandler = (event) => {
+      this.setState({
+        error: event.reason.message || event.reason || 'Unknown Async Error',
+        errorInfo: event.reason
+      });
+      // You can also log error messages to an error reporting service here
+    };
+  }
+
+  // Listen for Async Errors
+  componentDidMount() {
+    window.addEventListener("unhandledrejection", this.asyncHandler);
+  }
+
+  // Cleanup listener
+  componentWillUnmount() {
+    window.removeEventListener("unhandledrejection", this.asyncHandler);
   }
   
+  // Catch errors in any child components
   componentDidCatch(error, errorInfo) {
-    // Catch errors in any components below and re-render with error message
     this.setState({
       error: error,
       errorInfo: errorInfo
@@ -24,8 +43,8 @@ class ErrorBoundary extends Component {
     return (
       <ErrorStyle title="Something went wrong.">
         {this.state.error && this.state.error.toString()}
-        <br />
-        {this.state.errorInfo.componentStack}
+        <br /> <br />
+        {this.state.errorInfo.componentStack || this.state.errorInfo.stack || this.state.errorInfo.toString()}
       </ErrorStyle>
     );
   }  
