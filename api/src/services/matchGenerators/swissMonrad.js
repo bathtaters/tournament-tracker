1c26bfe59237e66515648d335ea0a6ab1aea0a47
bathtaters <nomoremrnice7@gmail.com>
2022-03-03 04:30:54 -0500

    complete rework to fix match pairing

--- api/src/services/matchGenerators/swissMonrad.js
+++ api/src/services/matchGenerators/swissMonrad.js
@@ -1,76 +1,64 @@
-// Get matchups for new round using Swiss Monrad Alogrithm
-// => Match Table: (2D ID Array) [ [ playerids in match1 ], [ match2 ], ...  ]
-const { unflat, splicing, anyElements, revReplace2dIndex } = require('./matchGen.utils');
+const { avg, diff, count, getGroups, getCombos, randomGroup } = require('./matchGen.utils');
 
-// -- Generate Match Table: Swiss Monrad Alogrithm -- //
 
-function generateMatchups(ranking, { playerspermatch, byes, allOpps }) {
-    // Generate simple matchups (w/o drops)
-    let matches = unflat(ranking, playerspermatch);
-    
-    // Don't allow players to have multiple byes
-    if (byes) {
-        for (let i = matches.length; i-- > 0; ) { // mutate in reverse
-            if (matches[i].length === 1 && byes.includes(matches[i][0])) {
-                // Find the nearest swap w/o a bye
-                const idx = revReplace2dIndex(matches, i, m => !byes.includes(m));
-                if (idx) {
-                    [
-                        matches[i][0],
-                        matches[idx[0]][idx[1]]
-                    ] = [
-                        matches[idx[0]][idx[1]],
-                        matches[i][0]
-                    ];
-                }
-                // If no replacement is found, player stuck w/ 2nd bye
-            }
-        }
-    }
+// --- SPECIFIC SETTINGS --- \\
 
-    // Don't allow rematches
-    if (allOpps) {
-        for (let i = matches.length; i-- > 0; ) {
-            for (let j = matches[i].length; j-- > 0; ) {
-                const playerid = matches[i][j];
-                const playerOpps = allOpps[playerid] && allOpps[playerid].oppids;
-
-                if (playerOpps && anyElements(matches[i], playerOpps)) {
-                    // Find a swap who won't cause another rematch
-                    const idx = revReplace2dIndex(
-                        matches, i,
-                        isUniqueMatchup(playerOpps, allOpps, splicing(matches[i], j))
-                    );
-                    if (idx) {
-                        [
-                            matches[i][j],
-                            matches[idx[0]][idx[1]]
-                        ] = [
-                            matches[idx[0]][idx[1]],
-                            playerid
-                        ];
-                    }
-                    // If no replacement is found, rematch will happen
-                }
-            }
-        }
-    }
-    return matches;
+// Stats weighting for calculations
+const weight = {
+  penalty:   50000000, // Per rematched player or x2 for each bye
+  matchRate:  1000000,
+  oppMatch:     10000,
+  gameRate:       100,
+  oppGame:          1,
 }
 
+// Calculate single player's base score
+const getPlayerScore = (stats) => !stats ? 0 :
+  Object.keys(weight).reduce((tot,key) => tot + (key in stats ? stats[key] * weight[key] : 0), 0)
 
-// Callback for checking for unique matchups
-const isUniqueMatchup = (playerOpps, allOpps, matchMinusPlayer) => (testId, testMatch, testIdx) =>
-    !anyElements(                   // No overlap between:
-        splicing(testMatch, testIdx),   // test player's current opponents
-        playerOpps                      // current player's prior opponents
-    ) && (                          // AND
-        !allOpps[testId] ||             // test player doesn't have prior opponents
-        !anyElements(                   // OR No overlap between:
-            matchMinusPlayer,               // current player's current opponents
-            allOpps[testId].oppids          // test player's prior opponents
-        )
-    );
+// Calculate score of 2 players (base + rematch penalties)
+const getComboScore = (scores, opps) => ([playerA, playerB]) =>
+  diff(scores[playerA], scores[playerB]) + (count(playerB, opps[playerA]) + count(playerA, opps[playerB])) * weight.penalty
+
+// Calculate score for single player match (base + bye penalties)
+const getSoloScore = (player, score, byes) => score + count(player, byes) * weight.penalty * 2
+
+// Uncomment to enable logging of each combo's score for debugging
+const logScores = (sc,gp,bs) => null //gp.forEach((e,i) => console.log(sc[i] === bs ? 'x' : '-', sc[i].toFixed(0), e))
+
+
+
+// --- MAIN --- \\
+
+function generateMatchups(stats, { playerspermatch, byes, oppData }) {
+  // Randomize matches for initial pairing
+  if (stats.noStats) return randomGroup(stats.ranking, playerspermatch)
+
+  // Generate all possible match groupings
+  const allPossible = getGroups(stats.ranking, playerspermatch)
+
+  // Calculate base player scores
+  const playerScores = stats.ranking.reduce((scores, player) => 
+    Object.assign(scores, { [player]: getPlayerScore(stats[player]) }),
+  {})
+
+  // Calculate scores for every possible pairing
+  const allScores = allPossible.map((matchList) =>
+    avg(matchList.map((match) =>
+      // Bye match
+      match.length === 1 ? getSoloScore(match[0], playerScores[match[0]], byes) :
+
+      // Standard match (average out scores of each player pair)
+      avg( getCombos(match, 2).map(getComboScore(playerScores, oppData)) )
+    ))
+  )
+
+  // DEBUG LOG
+  logScores(allScores, allPossible, Math.min(...allScores))
+
+  // Return pairing with the lowest score
+  return allPossible[allScores.indexOf(Math.min(...allScores))]
+}
 
 
 module.exports = generateMatchups;
