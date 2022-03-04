439d103a6b5f059a3a99fbc43f0d596cd8929a4f
bathtaters <nomoremrnice7@gmail.com>
2022-03-04 00:51:16 -0500

    update testing for overhauled module

--- api/src/services/matchGenerators/matchGen.utils.test.js
+++ api/src/services/matchGenerators/matchGen.utils.test.js
@@ -1,119 +1,307 @@
-const { splicing, anyElements, unflat, revReplace2dIndex } = require('./matchGen.utils');
+const warnSpy = jest.spyOn(global.console, "warn");
+const {
+  count, sum, avg, diff,
+  deepEqual, deepSwap, removeDupes,
+  getCombos, getGroups, randomGroup
+} = require('./matchGen.utils');
 
-// ---- Splice-In-Place ---- //
-
-describe('splicing', () => {
-  it('returns spliced array', () => {
-    expect(splicing(['a','b','c','d'], 2, 1)).toEqual(['a','b','d']);
-  });
-  it('removes multiple entries', () => {
-    expect(splicing(['a','b','c','d'], 1, 2)).toEqual(['a','d']);
-  });
-  it('doesn\'t mutate input array', () => {
-    let array = ['a','b','c','d'];
-    const result = splicing(array, 1, 2);
-    expect(result).not.toBe(array);
-    expect(array).toEqual(['a','b','c','d']);
-  });
-
-  it('index of 0 unshifts', () => {
-    expect(splicing(['a','b','c','d'], 0, 1)).toEqual(['b','c','d']);
-  });
-  it('index > length copies only', () => {
-    expect(splicing(['a','b','c','d'], 5, 1)).toEqual(['a','b','c','d']);
-  });
-  it('count of 0 copies only', () => {
-    expect(splicing(['a','b','c','d'], 2, 0)).toEqual(['a','b','c','d']);
-  }); // try multiple idx vals
-});
+// console.warn setup
+afterAll(() => { warnSpy.mockRestore(); });
 
 
-// ---- Test Common Elements ---- //
+// ---- Basic Math ---- //
+describe('mathOps', () => {
 
-describe('anyElements', () => {
-  it('no common elements = false', () => {
-    expect(anyElements(['a','b','c'],['d','e','f'])).toBe(false);
-  });
-  it('one common element = true', () => {
-    expect(anyElements(['a','b','c'],['d','e','b'])).toBe(true);
-  });
-  it('all common elements = true', () => {
-    expect(anyElements(['a','b','c'],['a','b','c'])).toBe(true);
-  });
-  it('empty array = false', () => {
-    expect(anyElements([],['d','e','f'])).toBe(false);
-    expect(anyElements(['a','b','c'],[])).toBe(false);
-    expect(anyElements([],[])).toBe(false);
-  });
-});
+  // COUNT
+  describe('count', () => {
+    it('counts', () => {
+      expect(count('a', ['a','b','c','a','b','a'])).toBe(3)
+      expect(count('c', ['a','b','c','a','b','a'])).toBe(1)
+    })
+    it('works with empty array', () => {
+      expect(count('a', [])).toBe(0)
+      expect(count('c', [])).toBe(0)
+    })
+    it('works with missing item', () => {
+      expect(count('d', ['a','b','c','a','b','a'])).toBe(0)
+      expect(count('',  ['a','b','c','a','b','a'])).toBe(0)
+    })
+  })
+
+  // SUM
+  describe('sum', () => {
+    it('sums', () => {
+      expect(sum([1, 3, 6, 2, 4])).toBe(16)
+      expect(sum([10, 20, 20, 5])).toBe(55)
+    })
+    it('works with single element', () => {
+      expect(sum([13])).toBe(13)
+    })
+    it('works with empty array', () => {
+      expect(sum([])).toBe(0)
+    })
+  })
+
+  // AVERAGE
+  describe('avg', () => {
+    it('averages', () => {
+      expect(avg([3, 6, 2, 5, 4])).toBeCloseTo(4)
+      expect(avg([10, 23, 17, 5])).toBeCloseTo(13.75)
+    })
+    it('works with single element', () => {
+      expect(avg([13])).toBe(13)
+    })
+    it('works with empty array', () => {
+      expect(avg([])).toBe(0)
+    })
+  })
+
+  // DIFFERENCE
+  describe('diff', () => {
+    it('standard difference', () => {
+      expect(diff( 4,  2)).toBe( 2)
+      expect(diff( 9,  1)).toBe( 8)
+      expect(diff(35, 24)).toBe(11)
+    })
+    it('absolute difference', () => {
+      expect(diff( 2,  4)).toBe( 2)
+      expect(diff( 1,  9)).toBe( 8)
+      expect(diff(12, 31)).toBe(19)
+    })
+    it('works when difference is 0', () => {
+      expect(diff( 0,  0)).toBe(0)
+      expect(diff( 4,  4)).toBe(0)
+      expect(diff(13, 13)).toBe(0)
+    })
+  })
+})
 
 
-// ---- 2D-ize Array ---- //
+// ---- Array Operations ---- //
+describe('arrayOps', () => {
 
-describe('unflat', () => {
+  // DEEP EQUAL
+  describe('deepEqual', () => {
+    it('works with non-array', () => {
+      expect(deepEqual('test','test' )).toBe(true)
+      expect(deepEqual('test','test+')).toBe(false)
+    })
+    it('works with 1D array', () => {
+      expect(deepEqual(['a','b','c','d'],['a','b','c','d'])).toBe(true)
+      expect(deepEqual(['a','b','c','d'],['a','b','e','d'])).toBe(false)
+      expect(deepEqual(['a','b','c','d'],['a','d','c','b'])).toBe(false)
+      expect(deepEqual(['a','b','c'],['a','b','c','d'])).toBe(false)
+    })
+    it('works with 2D array', () => {
+      expect(deepEqual([[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]])).toBe(true)
+      expect(deepEqual([[1,2,3],[4,5,6]],[[1,2,3],[4,6,5]])).toBe(false)
+      expect(deepEqual([[1,2],[4,5,6]],[[1,3],[4,5,6]])).toBe(false)
+    })
+    it('works with multi-D array', () => {
+      expect(deepEqual(['a',[1,2],[[3],[['b']]]],['a',[1,2],[[3],[['b' ]]]])).toBe(true)
+      expect(deepEqual(['a',[1,2],[[3],[['b']]]],['a',[1,2],[[4],[['b' ]]]])).toBe(false)
+      expect(deepEqual(['a',[1,2],[[3],[['b']]]],['a',[1,2],[[3],[['ba']]]])).toBe(false)
+      expect(deepEqual(['a',[1,2],[[3],[['b']]]],['a',[1,2],[[3], ['b' ]]] )).toBe(false)
+      expect(deepEqual(['a',[1,2],[[3],[['b']]]], null)).toBe(false)
+    })
+  })
+
+  // DEEP SWAP
+  describe('deepSwap', () => {
+    it('works with non-array', () => {
+      expect(deepSwap('test', {test: 'swap'})).toBe('swap')
+      expect(deepSwap('test-2', {test: 'swap'})).toBe('test-2')
+    })
+    it('works with 1D array', () => {
+      expect(deepSwap(['a','b','c','d'], {b:'test', c:'swap'}))
+        .toEqual(['a','test','swap','d'])
+    })
+    it('works with 2D array', () => {
+      expect(deepSwap([['a','b'],['c','d']], {b:'swap', d:'test'}))
+        .toEqual([['a','swap'], ['c','test']])
+    })
+    it('works with multi-D array', () => {
+      expect(deepSwap(['a',[1,2],[[3],[['b']]]], {b:'test', '2':'swap'}))
+        .toEqual(['a',[1,'swap'],[[3],[['test']]]])
+    })
+    it('empty dict does nothing', () => {
+      expect(deepSwap(['a','b',['c','d']], {})).toEqual(['a','b',['c','d']])
+    })
+    it('returns copy of array', () => {
+      let array = ['a','b',['c','d']]
+      expect(deepSwap(array, {})).not.toBe(array)
+      expect(array).toEqual(['a','b',['c','d']])
+    })
+  })
+
+  // REMOVE DUPES
+  describe('removeDupes', () => {
+    it('removes a dupe', () => {
+      expect(removeDupes(['a','b','b','c'])).toEqual(['a','b','c'])
+    })
+    it('removes +1 dupes', () => {
+      expect(removeDupes(['a','b','c','c','c','c'])).toEqual(['a','b','c'])
+    })
+    it('removes multiple dupes', () => {
+      expect(removeDupes(['a','a','b','c','c'])).toEqual(['a','b','c'])
+    })
+    it('removes +1 of multiple dupes', () => {
+      expect(removeDupes(['a','a','a','a','b','b','c'])).toEqual(['a','b','c'])
+    })
+    it('detects deep equality', () => {
+      expect(removeDupes([['a','b'],['b','c'],['b','c'],['c','d']]))
+        .toEqual([['a','b'],['b','c'],['c','d']])
+      expect(removeDupes([1,[2,3],[2,3],[2,3],[4,5,6]]))
+        .toEqual([1,[2,3],[4,5,6]])
+    })
+  })
+})
+
+
+// ---- Combination Operations ---- //
+describe('combinationOps', () => {
+
+  // GET COMBOS
+  describe('getCombos', () => {
+    it('gets combinations of size 1', () => {
+      expect(getCombos(['a','b'],         1)).toEqual([['a'],['b']])
+      expect(getCombos(['a','b','c'],     1)).toEqual([['a'],['b'],['c']])
+      expect(getCombos(['a','b','c','d'], 1)).toEqual([['a'],['b'],['c'],['d']])
+    })
+    it('gets combinations of size 2', () => {
+      expect(getCombos(['a','b','c'], 2)).toEqual([['a','b'], ['a','c'], ['b','c']])
+      expect(getCombos(['a','b','c','d'], 2)).toEqual([
+        ['a','b'], ['a','c'], ['a','d'],
+        ['b','c'], ['b','d'], ['c','d'],
+      ])
+    })
+    it('gets combinations of size 3', () => {
+      expect(getCombos(['a','b','c','d'], 3)).toEqual([
+        ['a','b','c'], ['a','b','d'],
+        ['a','c','d'], ['b','c','d'],
+      ])
+      expect(getCombos(['a','b','c','d','e'], 3)).toEqual([
+        ['a','b','c'], ['a','b','d'], ['a','b','e'],
+        ['a','c','d'], ['a','c','e'], ['a','d','e'],
+        ['b','c','d'], ['b','c','e'], ['b','d','e'], ['c','d','e'],
+      ])
+    })
+    it('gets single combination', () => {
+      expect(getCombos(['a','b'],             2)).toEqual([['a','b']])
+      expect(getCombos(['a','b','c'],         3)).toEqual([['a','b','c']])
+      expect(getCombos(['a','b','c','d'],     4)).toEqual([['a','b','c','d']])
+      expect(getCombos(['a','b','c','d','e'], 5)).toEqual([['a','b','c','d','e']])
+    })
+    it('minimum width = 1', () => {
+      expect(getCombos(['a','b','c'],   0)).toEqual([['a'],['b'],['c']])
+      expect(getCombos(['a','b','c'],  -1)).toEqual([['a'],['b'],['c']])
+      expect(getCombos(['a','b','c'], -13)).toEqual([['a'],['b'],['c']])
+    })
+    it('single-element input array', () => {
+      expect(getCombos(['a'], 1)).toEqual([['a']])
+    })
+    it('warn when input array smaller than width', () => {
+      warnSpy.mockImplementationOnce(()=>{}).mockImplementationOnce(()=>{})
+
+      expect(getCombos([],            -1)).toEqual([])
+      expect(getCombos(['a','b','c'], 12)).toEqual([])
+      expect(warnSpy).toBeCalledTimes(2)
+      
+      expect(warnSpy).toHaveBeenNthCalledWith(1,
+        'getCombos array is smaller than width', -1, 0, expect.any(Array)
+      )
+      expect(warnSpy).toHaveBeenNthCalledWith(2,
+        'getCombos array is smaller than width', 12, 3, expect.any(Array)
+      )
+    })
+  })
+
+  // GET GROUPS
+  describe('getGroups', () => {
+    it('gets groups of size 1', () => {
+      expect(getGroups(['a','b'], 1)).toEqual([[['a'],['b']]])
+      expect(getGroups(['a','b','c','d'], 1)).toEqual([[['a'],['b'],['c'],['d']]])
+    })
+    it('gets groups of size 2', () => {
+      expect(getGroups(['a','b','c','d'], 2)).toEqual([
+        [['a','b'],['c','d']], [['a','c'],['b','d']], [['a','d'],['b','c']]
+      ])
+    })
+    it('gets groups of size 3', () => {
+      expect(getGroups(['a','b','c','d','e','f'], 3)).toEqual([
+        [['a','b','c'], ['d','e','f']], [['a','b','d'], ['c','e','f']],
+        [['a','b','e'], ['c','d','f']], [['a','b','f'], ['c','d','e']],
+        [['a','c','d'], ['b','e','f']], [['a','c','e'], ['b','d','f']],
+        [['a','c','f'], ['b','d','e']], [['a','d','e'], ['b','c','f']],
+        [['a','d','f'], ['b','c','e']], [['a','e','f'], ['b','c','d']],
+      ])
+    })
+    it('get uneven group sizes', () => {
+      expect(getGroups(['a','b','c'], 2)).toEqual([
+        [['a','b'],['c']], [['a'],['b','c']], [['a','c'],['b']],
+      ])
+      expect(getGroups(['a','b','c','d'], 3)).toEqual([
+        [['a','b','c'],['d']], [['a'],['b','c','d']], 
+        [['a','b','d'],['c']], [['a','c','d'],['b']], 
+      ])
+    })
+    it('get single group', () => {
+      expect(getGroups(['a','b'],         2)).toEqual([[['a','b']]])
+      expect(getGroups(['a','b','c'],     3)).toEqual([[['a','b','c']]])
+      expect(getGroups(['a','b','c','d'], 4)).toEqual([[['a','b','c','d']]])
+    })
+    it('minimum width = 1', () => {
+      expect(getGroups(['a','b','c','d'],   0)).toEqual([[['a'],['b'],['c'],['d']]])
+      expect(getGroups(['a','b','c','d'],  -1)).toEqual([[['a'],['b'],['c'],['d']]])
+      expect(getGroups(['a','b','c','d'], -13)).toEqual([[['a'],['b'],['c'],['d']]])
+    })
+    it('using input smaller than width', () => {
+      expect(getGroups(['a','b'],          3)).toEqual([[['a','b']]])
+      expect(getGroups(['a','b','c','d'],  8)).toEqual([[['a','b','c','d']]])
+      expect(getGroups(['a'], 12)).toEqual([[['a']]])
+    })
+    it('using input of 1 element', () => {
+      expect(getGroups(['a'],  1)).toEqual([[['a']]])
+      expect(getGroups(['a'],  3)).toEqual([[['a']]])
+      expect(getGroups(['a'], 12)).toEqual([[['a']]])
+    })
+    it('using empty input', () => {
+      expect(getGroups([],  1)).toEqual([[]])
+      expect(getGroups([],  3)).toEqual([[]])
+      expect(getGroups([], 12)).toEqual([[]])
+    })
+  })
+})
+
+// ---- Randomize & 2D-ize Array ---- //
+describe('randomGroup', () => {
+  const input  = ['a','b','c','d']
+  const output = expect.stringMatching(RegExp('^['+input.join('')+']$'))
+
   it('elemSize of 2', () => {
-    expect(unflat(['a','b','c','d'], 2)).toEqual([['a','b'],['c','d']]);
+    expect(randomGroup(input, 2)).toEqual([[output,output],[output,output]]);
   });
   it('elemSize of 1', () => {
-    expect(unflat(['a','b','c'], 1)).toEqual([['a'],['b'],['c']]);
+    expect(randomGroup(input, 1)).toEqual([[output],[output],[output],[output]]);
   });
   it('elemSize of 0 copies array', () => {
-    expect(unflat(['a','b','c'], 0)).toEqual(['a','b','c']);
+    expect(randomGroup(input, 0)).toEqual(input.map(()=>output));
   });
   it('elemSize is array length', () => {
-    expect(unflat(['a','b','c'], 3)).toEqual([['a','b','c']]);
+    expect(randomGroup(input, 4)).toEqual([input.map(()=>output)]);
   });
   it('elemSize not factor of array length', () => {
-    expect(unflat(['a','b','c'], 2)).toEqual([['a','b'],['c']]);
+    expect(randomGroup(input, 3)).toEqual([[output,output,output],[output]]);
   });
   it('empty array passes through', () => {
-    expect(unflat([], 1)).toEqual([]);
-    expect(unflat([], 3)).toEqual([]);
-    expect(unflat([], 0)).toEqual([]);
+    expect(randomGroup([], 1)).toEqual([]);
+    expect(randomGroup([], 3)).toEqual([]);
+    expect(randomGroup([], 0)).toEqual([]);
   });
   it('doesn\'t mutate array', () => {
-    let array = ['a','b','c'];
-    const result = unflat(array, 1);
+    let array = input.slice();
+    const result = randomGroup(array, 1);
     expect(result).not.toBe(array);
-    expect(array).toEqual(['a','b','c']);
-  });
-});
-
-
-// ---- Get Replacement Index of 2D Array ---- //
-
-describe('revReplace2dIndex', () => {
-  const array = [['a','b'],['c','d'],['e','f']];
-  const matches = match => val => match === val;
-
-  it('matches starting from end', () => {
-    expect(revReplace2dIndex(array, 2, matches('b')))
-    .toEqual([0,1]);
-  });
-
-  it('matches starting from start', () => {
-    expect(revReplace2dIndex(array, 0, matches('d')))
-      .toEqual([1,1]);
-  });
-
-  it('loops around from middle to match', () => {
-    expect(revReplace2dIndex(array, 1, matches('e')))
-      .toEqual([2,0]);
-  });
-
-  it('returns undefined when no match', () => {
-    expect(revReplace2dIndex(array, 2, matches('g')))
-      .toBeUndefined();
-  });
-
-  it('doesn\'t match item w/ startIdx', () => {
-    expect(revReplace2dIndex(array, 1, matches('c')))
-      .toBeUndefined();
-  });
-
-  it('isReplace recieves correct args', () => {
-    const matchMock = jest.fn(() => true);
-    revReplace2dIndex(array, 2, matchMock);
-    expect(matchMock).toBeCalledWith('d',['c','d'],1);
+    expect(array).toEqual(input);
   });
 });
--- api/src/services/matchGenerators/swissMonrad.test.js
+++ api/src/services/matchGenerators/swissMonrad.test.js
@@ -1,35 +1,73 @@
-const generateMatchups = require('./swissMonrad');
+const generateMatchups = require('./swissMonrad')
 
 // NOTE: Uses un-mocked matchGen.utils, ensure that passes test first //
 
-const normArr = arr => arr.map(e => e.sort()).sort(); // normalize 2d arrays
-
 describe('generateMatchups', () => {
-  const ranking = ['a','b','c','d'];
+  const stats = {
+    a: { matchRate: 0.7, oppMatch: 0.2, gameRate: 0.9, oppGame: 0.3 },
+    b: { matchRate: 0.5, oppMatch: 0.5, gameRate: 0.7, oppGame: 0.4 },
+    c: { matchRate: 0.5, oppMatch: 0.4, gameRate: 0.7, oppGame: 0.3 },
+    d: { matchRate: 0.5, oppMatch: 0.4, gameRate: 0.3, oppGame: 0.3 },
+    e: { matchRate: 0.5, oppMatch: 0.4, gameRate: 0.3, oppGame: 0.2 },
+    f: { matchRate: 0.5, oppMatch: 0.4, gameRate: 0.3, oppGame: 0.2 },
+    ranking: ['a','b','c','d'],
+  }
+  const fullStats = { ...stats, ranking: stats.ranking.concat('e','f') }
+  const blankStats = { ranking: stats.ranking, noStats: true }
 
-  it('pairs without additional data', () => {
-    expect(generateMatchups(ranking, { playerspermatch: 2 }))
-      .toEqual([['a','b'],['c','d']]);
-  });
+  it('pairs using stats to rank', () => {
+    expect(generateMatchups(stats, { playerspermatch: 2 }))
+      .toEqual([['a','b'],['c','d']])
+  })
+
+  it('randomly pairs w/o data', () => {
+    const anyone = expect.stringMatching(/^[abcd]$/)
+    expect(generateMatchups(blankStats, { playerspermatch: 2 }))
+      .toEqual([[anyone,anyone],[anyone,anyone]])
+  })
 
   it('pairs with uneven playerCount', () => {
-    expect(generateMatchups(ranking, { playerspermatch: 3 }))
-      .toEqual([['a','b','c'],['d']]);
-  });
+    expect(generateMatchups(stats, { playerspermatch: 3 }))
+      .toEqual([['a','b','c'],['d']])
+  })
 
-  it('doesn\'t allow repeat byes', () => {
-    expect(generateMatchups(ranking, {
+  it('no repeat byes', () => {
+    expect(generateMatchups(stats, {
       playerspermatch: 3,
       byes: ['d','c']
-    })).toEqual([['a','d','c'],['b']]);
-  });
+    })).toEqual([['a','c','d'],['b']])
+  })
 
-  it('doesn\'t allow repeat matchups', () => {
-    expect( normArr(
-      generateMatchups(ranking, {
+  it('no repeat matchups', () => {
+    expect(
+      generateMatchups(fullStats, {
         playerspermatch: 2,
-        allOpps: { a : { oppids: ['d','b'] }, b : { oppids: ['a'] } }
+        oppData: { a: ['f','e','d','b'], b: ['a','d'], c: ['f'], d: ['f','b','a'], e: ['f','a'], f: ['a','e','d','c'] }
       })
-    )).toEqual( normArr([['a','c'], ['b','d']]));
-  });
-});
+    ).toEqual([['a','c'], ['b','f'], ['d','e']])
+  })
+
+  it('least amount of repeat byes', () => {
+    expect(generateMatchups(stats, {
+      playerspermatch: 3,
+      byes: ['a','b','c','d','d','d','c','c','b','a']
+    })).toEqual([['a','c','d'],['b']])
+  })
+
+  it('least amount of repeat matchups', () => {
+    
+    expect(
+      generateMatchups(fullStats, {
+        playerspermatch: 2,
+        oppData: {
+          a: ['f','f','f','e','e','d','d','d','b','c'],
+          b: ['a','d','d','d','c','e','f'],
+          c: ['f','f','b','a','d'],
+          d: ['f','f','b','b','b','a','a','a','c'],
+          e: ['f','f','f','a','a','b'],
+          f: ['a','a','a','e','e','e','d','d','c','c','b']
+        }
+      })
+    ).toEqual([['a','c'], ['b','f'], ['d','e']])
+  })
+})
