933f1407069bcf7a64e803cba2ef7840cfa1ef52
bathtaters <nomoremrnice7@gmail.com>
2022-03-05 01:44:46 -0500

    refetch master stats on round +/-

--- client/src/pages/event/event.fetch.js
+++ client/src/pages/event/event.fetch.js
@@ -12,14 +12,14 @@ export const eventApi = fetchApi.injectEndpoints({
     nextRound: build.mutation({
       query: id => ({ url: `event/${id}/round`, method: 'POST' }),
       transformResponse: res => console.log('ROUND+',res) || res,
-      invalidatesTags: getTags(['Event','Match','Stats'], {all:0,addBase:['PlayerDetail']}),
+      invalidatesTags: getTags(['Event','Match','Stats'], {all:0,addBase:['PlayerDetail'],addAll:['Stats']}),
       onQueryStarted: nextRoundUpdate,
     }),
 
     clearRound: build.mutation({
       query: id => ({ url: `event/${id}/round`, method: 'DELETE' }),
       transformResponse: res => console.log('ROUND-',res) || res,
-      invalidatesTags: getTags(['Event','Match','Stats'], {all:0,addBase:['PlayerDetail']}),
+      invalidatesTags: getTags(['Event','Match','Stats'], {all:0,addBase:['PlayerDetail'],addAll:['Stats']}),
       onQueryStarted: clearRoundUpdate,
     }),
 
