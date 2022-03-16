7a8eb3c4052822c1f376592aab2c904c9e574b55
bathtaters <nomoremrnice7@gmail.com>
2022-03-16 16:52:23 -0400

    update drag & drop rules for schedule slots

--- client/src/pages/schedule/services/day.services.js
+++ client/src/pages/schedule/services/day.services.js
@@ -3,11 +3,24 @@ import { dragType } from "../../../assets/strings";
 export const dataType = dragType.event;
 
 // Drag & Drop tester
-export const canDrop = types => types.includes(dataType);
+export const canDrop = (types, target, self) => types.includes(dataType) && (target !== noDate || self !== noDate);
 
-// Drop handler factory
+// Drop handler
+// a & b = { id, day, slot } or { day: noDate } (Unscheduled box)
 export const dropController = (updateEvent) => (a,b) => {
-  [a.day, b.day] = [b.day, a.day].map(d => d === noDate ? null : d);
+  // Skip rules
+  if (a.id === b.id || b.day === noDate && a.day === noDate) return;
+  
+  // Swap data
+  swapKeys(a,b,'day');
+  swapKeys(a,b,'slot');
+  [a,b].map(d => { if (!d.slot) delete d.slot })
+  
+  // Fetch updates
   updateEvent(a);
   if (b.id) updateEvent(b);
 }
+
+// Drop Helper
+const swapKeys = (a,b,key) =>
+  [a[key], b[key]] = [b[key], a[key]].map(d => key === 'day' && d === noDate ? null : d);
