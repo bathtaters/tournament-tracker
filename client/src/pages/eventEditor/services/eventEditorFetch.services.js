ea29b5bd63961d227a061db91e3653934f3bc3d8
bathtaters <nomoremrnice7@gmail.com>
2022-03-16 16:50:34 -0400

    fix schedule optimistic updates to work with slots

--- client/src/pages/eventEditor/services/eventEditorFetch.services.js
+++ client/src/pages/eventEditor/services/eventEditorFetch.services.js
@@ -1,30 +1,16 @@
 import { fetchApi } from '../../common/common.fetch';
+import { updateSchedule } from '../../schedule/services/scheduleFetch.services';
 import { nextTempId } from './eventEditor.services';
-
-// Change Date of Event - Helper for eventUpdate
-function swapToDay(schedule, id, newDay) {
-  // Remove old
-  for (const day in schedule) {
-    const idx = (schedule[day].events || []).indexOf(id);
-    if (idx >= 0) { 
-      schedule[day].events.splice(idx,1); break;
-    }
-  }
-  // Add new
-  if (!schedule[newDay]) schedule[newDay] = { events: [id] };
-  else if (!schedule[newDay].events) schedule[newDay].events = [id];
-  else schedule[newDay].events.push(id);
-}
 
 // Update cache for new event
 function createUpdate(body, { dispatch, getState }) {
   const events = getState().dbApi.queries['event(undefined)'];
   const id = nextTempId('EVENT', events && Object.keys(events));
-  dispatch(fetchApi.util.updateQueryData('event', undefined, draft => { draft[id] = body; }));
+  dispatch(fetchApi.util.updateQueryData('event', undefined, draft => {
+    draft[id] = body;
+  }));
   dispatch(fetchApi.util.updateQueryData('schedule', undefined, draft => { 
-    if (!draft.none) draft.none = {};
-    if (!draft.none.events) draft.none.events = [];
-    draft.none.events.push(id);
+    updateSchedule(draft, id, body);
   }));
 };
 
@@ -36,9 +22,10 @@ function eventUpdate({ id, ...body }, { dispatch }) {
   dispatch(fetchApi.util.updateQueryData('event', id, draft => { 
     Object.assign(draft, body); 
   }));
-  if ('day' in body) {
+
+  if ('day' in body || 'slot' in body) {
     dispatch(fetchApi.util.updateQueryData('schedule', undefined, draft => {
-      swapToDay(draft, id, body.day);
+      updateSchedule(draft, id, body);
     }));
   }
 };
