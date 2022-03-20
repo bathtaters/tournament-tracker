03148301a02f64de8d190c195e19cca2bf5fd0fb
bathtaters <nomoremrnice7@gmail.com>
2022-03-20 19:26:14 -0400

    move drop controller + handler into single hook

--- client/src/pages/schedule/services/day.services.js
+++ client/src/pages/schedule/services/day.services.js
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+import { useSetEventMutation } from "../schedule.fetch";
 import { noDate } from "./date.utils";
 import { dragType } from "../../../assets/strings";
 export const dataType = dragType.event;
@@ -7,18 +9,23 @@ export const canDrop = (types, target, self) => types.includes(dataType) && (tar
 
 // Drop handler
 // a & b = { id, day, slot } or { day: noDate } (Unscheduled box)
-export const dropController = (updateEvent) => (a,b) => {
-  // Skip rules
-  if (a.id === b.id || b.day === noDate && a.day === noDate) return;
-  
-  // Swap data
-  swapKeys(a,b,'day');
-  swapKeys(a,b,'slot');
-  [a,b].map(d => { if (!d.slot) delete d.slot })
-  
-  // Fetch updates
-  updateEvent(a);
-  if (b.id) updateEvent(b);
+export function useUpdateSchedule() {
+  const [ updateEvent ] = useSetEventMutation()
+
+  return useCallback((a,b) => {
+    // Skip rules
+    if (a.id === b.id || b.day === noDate && a.day === noDate) return;
+    
+    // Swap data
+    swapKeys(a,b,'day');
+    swapKeys(a,b,'slot');
+    [a,b].map(d => { if (!d.slot) delete d.slot });
+    
+    // Fetch updates
+    updateEvent(a)
+    if (b.id) updateEvent(b)
+
+  }, [updateEvent, noDate])
 }
 
 // Drop Helper
