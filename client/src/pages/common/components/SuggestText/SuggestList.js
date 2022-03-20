ee2eb7049a1ab8e811fe4a872ec6a5dc1041a610
bathtaters <nomoremrnice7@gmail.com>
2022-03-20 16:30:39 -0400

    implement scrollTo hook for browsing list

--- client/src/pages/common/components/SuggestText/SuggestList.js
+++ client/src/pages/common/components/SuggestText/SuggestList.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import { EntryStyle } from "../../styles/SuggestTextStyles";
+import { ListStyle, EntryStyle } from "../../styles/SuggestTextStyles";
+import { useScrollToRef } from "../../services/basic.services";
 import { getId } from "../../services/SuggestText/suggestText.utils"
 
 function SuggestList({ list, className, selected, pick, setSelected }) {
+  // Scroll hidden item into view
+  const listRef = useRef(null);
+  const entryRef = useScrollToRef({ rootRef: listRef, threshold: 0.75, block: 'nearest' });
+
   // Catch no list
   if (!Array.isArray(list)) return null;
 
@@ -12,16 +17,18 @@ function SuggestList({ list, className, selected, pick, setSelected }) {
   const handleClick = (val) => () => pick(val);
 
   // Render list
-  return list.map((entry, idx) => (
-    <EntryStyle className={`${className} ${entry.className || ''}`} isSelected={selected === idx} key={getId(entry)}>
+  return <ListStyle ref={listRef}>{
+    list.map((entry, idx) => (
+      <EntryStyle className={`${className} ${entry.className || ''}`} isSelected={selected === idx} key={getId(entry)}>
 
-      <div onMouseDown={handleClick(entry)} onMouseEnter={handleHover(idx)}>
+        <div onMouseDown={handleClick(entry)} onMouseEnter={handleHover(idx)} ref={selected === idx ? entryRef : null}>
+          
+          {entry.value}
 
-        {entry.value}
-
-      </div>
-    </EntryStyle>
-  ));
+        </div>
+      </EntryStyle>
+    ))
+  }</ListStyle>;
 }
 
 export default SuggestList;
