7e8e9c08b992de301db2a42a4821a4fe90d64178
bathtaters <nomoremrnice7@gmail.com>
2022-03-20 19:29:31 -0400

    update adapter/updater for settings.daySlots=0

--- client/src/pages/schedule/services/scheduleFetch.services.js
+++ client/src/pages/schedule/services/scheduleFetch.services.js
@@ -1,41 +1,33 @@
-import { getDays, noDate } from "./date.utils"
+import { getDays, noDate, sortedEvents } from "./date.utils"
 
 // Convert Schedule output from server to array
 export function scheduleAdapter({ schedule, settings }) {
   // Skip if no data
-  if (!schedule || !settings) return console.log('SCHEDULE', {schedule, settings})
+  if (!schedule || !settings) return console.log('SCHEDULE MISSING', {schedule, settings})
   
   // Get base data
   const emptyDay = [...Array(settings.dayslots).values()]
   const dateRange = getDays(settings.datestart, settings.dateend)
 
   // Setup Unscheduled array
-  let output = [{ day: noDate, events: Object.keys(schedule[noDate]?.eventslots || {}) }]
+  let output = [{ day: noDate, events: sortedEvents(schedule[noDate]?.eventslots) }]
+  delete schedule[noDate]
 
   dateRange.forEach((day) => {
-    if (day === noDate) return // Skip Unscheduled
+    // Add entry for each day w/in range
+    output.push({ day, events: sortedEvents(schedule[day]?.eventslots, emptyDay) })
 
-    // Add events outside range to Unscheduled
-    if (!dateRange.includes(day))
-      return output[0].events.push(...Object.keys(schedule[day].eventslots))
-
-    // Setup day object base
-    const entry = { day, events: [...emptyDay] }
-    if (!schedule[day]?.eventslots) return output.push(entry) // Skip missing days
-    
-    for (const [event,slot] of Object.entries(schedule[day].eventslots)) {
-      // Add to scheduled slot
-      if (slot && !entry.events[slot - 1]) entry.events[slot - 1] = event
-      // Add unslotted to Unscheduled
-      else output[0].events.push(event)
-    }
-    
-    output.push(entry)
+    // Clear entry from schedule
+    delete schedule[day]
   })
+  
+  // Add remaining events to Unscheduled
+  Object.values(schedule).forEach(day => day.eventslots && output[0].events.push(...Object.keys(day.eventslots)))
 
   console.log('SCHEDULE', output)
   return output
 }
+
 
 // Change Date of Event - Helper for eventUpdate
 export function updateSchedule(schedule, id, { day, slot }) {
@@ -52,6 +44,7 @@ export function updateSchedule(schedule, id, { day, slot }) {
   
   // Add new
   const addIdx = !day ? -1 : schedule.findIndex(d => d.day === day);
-  if (addIdx < 0 || !slot) schedule[0].events.push(id);
-  else schedule[addIdx].events[slot - 1] = id;
+  if (addIdx < 0) schedule[0].events.push(id);
+  else if (slot) schedule[addIdx].events[slot - 1] = id;
+  else schedule[addIdx].events.push(id);
 }
