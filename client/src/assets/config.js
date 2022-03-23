432322c4988c2b0248629661d40afb115eea2fe0
bathtaters <nomoremrnice7@gmail.com>
2022-03-22 21:25:02 -0400

    aadd counter config

--- client/src/assets/config.js
+++ client/src/assets/config.js
@@ -1,6 +1,10 @@
 // Config settings
 
 export const suggestText = {
-  hideListWhenEmpty: false,
-  hideStaticWhenEmpty: true,
+  hideListWhenEmpty: false, // when textbox empty: false = show all / true = show none
+  hideStaticWhenEmpty: true, // when ^ is false, don't show 'static' options (ie. 'add player')
+}
+
+export const counter = {
+  updateDelay: 500, // ms delay before updates are pushed to server
 }
