c5cdfc335aae63cd44e13ee7f5bc2ea8ba10de45
bathtaters <nomoremrnice7@gmail.com>
2022-03-23 02:44:51 -0400

    fix error on missing player

--- client/src/pages/event/components/EventStats.js
+++ client/src/pages/event/components/EventStats.js
@@ -22,7 +22,7 @@ function EventStats({ event }) {
   if (isLoading || loadingPlayers || error || playerError)
     return <Loading loading={isLoading || loadingPlayers} error={error || playerError} className="text-xs mb-2" />;
 
-  const isRanked = data && data.ranking && data.ranking.length;
+  const isRanked = data?.ranking?.length;
   
   return (
     <EventStatsStyle title={isRanked ? 'Standings' : 'Players'}>
@@ -34,9 +34,9 @@ function EventStats({ event }) {
           <StatsRow
             rowNum={isRanked && idx + 1}
             id={pid}
-            name={players[pid].name}
-            isDrop={event.drops && event.drops.includes(pid)}
-            record={data[pid] && data[pid].matchRecord}
+            name={players[pid]?.name}
+            isDrop={event.drops?.includes(pid)}
+            record={data[pid]?.matchRecord}
             key={pid}
           />
         ) }
@@ -44,7 +44,7 @@ function EventStats({ event }) {
 
       <Modal ref={modal}>
         <h3 className="font-light max-color text-center mb-4">{event.title+' Stats'}</h3>
-        <Stats eventid={event.id} playerList={data && data.ranking} players={players} />
+        <Stats eventid={event.id} playerList={data?.ranking} players={players} />
       </Modal>
 
     </EventStatsStyle>
