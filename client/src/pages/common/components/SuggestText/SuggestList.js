5249772915e9a6c1d1b01f6495d96cb42823a5e9
bathtaters <nomoremrnice7@gmail.com>
2022-03-23 12:56:41 -0400

    change list to suggestions to simplify controller

--- client/src/pages/common/components/SuggestText/SuggestList.js
+++ client/src/pages/common/components/SuggestText/SuggestList.js
@@ -4,13 +4,13 @@ import { ListStyle, EntryStyle } from "../../styles/SuggestTextStyles";
 import { useScrollToRef } from "../../services/basic.services";
 import { getId } from "../../services/SuggestText/suggestText.utils"
 
-function SuggestList({ list, className, selected, pick, setSelected }) {
+function SuggestList({ suggestions, className, selected, pick, setSelected }) {
   // Scroll hidden item into view
   const listRef = useRef(null);
   const entryRef = useScrollToRef({ rootRef: listRef, threshold: 0.75, block: 'nearest' });
 
   // Catch no list
-  if (!Array.isArray(list)) return null;
+  if (!Array.isArray(suggestions)) return null;
 
   // Handlers
   const handleHover = (idx) => () => setSelected(idx);
@@ -18,7 +18,7 @@ function SuggestList({ list, className, selected, pick, setSelected }) {
 
   // Render list
   return <ListStyle ref={listRef}>{
-    list.map((entry, idx) => (
+    suggestions.map((entry, idx) => (
       <EntryStyle className={`${className} ${entry.className || ''}`} isSelected={selected === idx} key={getId(entry)}>
 
         <div onMouseDown={handleClick(entry)} onMouseEnter={handleHover(idx)} ref={selected === idx ? entryRef : null}>
