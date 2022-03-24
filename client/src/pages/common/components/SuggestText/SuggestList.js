13978365c5450df4695792aa7dce97cbbd621278
bathtaters <nomoremrnice7@gmail.com>
2022-03-24 14:24:46 -0400

    fix hover trigger on scroll event

--- client/src/pages/common/components/SuggestText/SuggestList.js
+++ client/src/pages/common/components/SuggestText/SuggestList.js
@@ -3,18 +3,21 @@ import React, { useRef } from "react";
 import { ListStyle, EntryStyle } from "../../styles/SuggestTextStyles";
 import { useScrollToRef } from "../../services/basic.services";
 import { getId } from "../../services/SuggestText/suggestText.utils"
+import { useSetOnHover } from "../../services/SuggestText/suggestText.services"
+
+
 
 function SuggestList({ suggestions, className, selected, pick, setSelected }) {
   // Scroll hidden item into view
   const listRef = useRef(null);
   const entryRef = useScrollToRef({ rootRef: listRef, threshold: 0.75, block: 'nearest' });
 
+  // Handlers
+  const handleHover = useSetOnHover(setSelected);
+  const handleClick = (val) => () => pick(val);
+
   // Catch no list
   if (!Array.isArray(suggestions)) return null;
-
-  // Handlers
-  const handleHover = (idx) => () => setSelected(idx);
-  const handleClick = (val) => () => pick(val);
 
   // Render list
   return <ListStyle ref={listRef}>{
