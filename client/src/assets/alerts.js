0ddde7b6fb07427969a3e0e0c86d11e8fb6df664
bathtaters <nomoremrnice7@gmail.com>
2022-03-29 17:28:51 -0400

    organize, improve wording & add titles to alerts

--- client/src/assets/alerts.js
+++ client/src/assets/alerts.js
@@ -1,68 +1,99 @@
-// Alerts
+// --- Constants --- \\
 const defPlayer = 'This player';
 
-export const modalCloseAlert = (customMsg) => ({
-  message: customMsg || "Are you sure you want to close? Unsaved changes will be lost.",
+
+// --- Generic Alerts --- \\
+
+export const modalCloseAlert = (customAlert = {}) => ({
+  title: "Close Window?",
+  message: "Unsaved changes will be lost.",
   buttons: ["Yes", "No"], // [0]=Y
+  ...customAlert
 })
+
+export const notLoadedAlert = { title: "Still Loading", message: "Try again in a minute or refresh." }
+
+
+// --- EventEditor Alerts --- \\
 
 export const deleteEventAlert = (title) => ({
   message: "Are you sure you want to delete " + (title || 'this event') + "? It will be lost for good.",
   buttons: [{ value: "Delete Event", className: "neg-bgd base-color-inv" }, "Cancel"], // [0]=Y
 })
 
-export const duplicatePlayerAlert = (name) => ({ message: (name || defPlayer) + " is already in this event." })
+export const duplicatePlayerAlert = (name) => ({
+  title: "Can't Add",
+  message: (name || defPlayer) + " is already here.",
+})
 
 export const createPlayerAlert = (name) => ({
-  message: (name || defPlayer) + " does not exist. Would you like to create a profile?",
-  buttons: ["Create", "Cancel"], // [0]=Y
+  title: "New Player?",
+  message: `Would you like to create a profile for ${name || defPlayer}?`,
+  buttons: [{ value: "Create",  className: "pos-bgd base-color-inv" }, "Cancel"], // [0]=Y
 })
 
 export const unsavedPlayerAlert = (name) => ({
-  message: (name || defPlayer) + " has not been added to the event. Would you like to add now?",
-  buttons: ["Add & Save", "Drop & Save", "Cancel"], // [0]=Y+, [1]=Y-, [2]=N
+  title: "Add Player?",
+  message: `Would you like to add ${name || defPlayer} before saving?`,
+  buttons: [ // [0]=Y+, [1]=Y-, [2]=N
+    { value: "Add",  className: "neg-bgd base-color-inv" },
+    { value: "Drop", className: "pos-bgd base-color-inv" },
+    "Back"
+  ],
 })
+
+
+// --- Event Alerts --- \\
 
 export const clearReportAlert = (title) => ({
-  message: `Are you sure you want to delete the records for ${title}?`,
-  buttons: ["Delete", "Cancel"], // [0]=Y
+  title: "Confirm Clear",
+  message: `This will delete match records for\n${title}.`,
+  buttons: [{ value: "Clear", className: "neg-bgd base-color-inv" }, "Cancel"], // [0]=Y
 })
-
-export const deletePlayerAlert = (name) => ({
-  message: "Are you sure you want to delete "+(name || defPlayer.toLowerCase())+"? All of their info will be lost.",
-  buttons: ["Delete", "Cancel"], // [0]=Y
-})
-
-export const duplicateNameAlert = (name) => ({ message: (name || defPlayer) + " already exists." })
-
-export const cantDeletePlayerAlert = (name) => ({ message: name+" cannot be deleted while registered for events." });
-
-export const emptyNameAlert = { message: "Please enter a name." }
-
-export const notLoadedAlert = { message: "Page data not loaded. Try again in a minute or refresh." }
 
 export const deleteRoundAlert = {
-  message: "Are you sure you want to delete this round? Reported matches will be lost for good.",
-  buttons: ["Yes", "No"], // [0]=Y
+  title: "Confirm Delete",
+  message: "Deleting this round will erase these matches for good.",
+  buttons: [{ value: "Delete", className: "neg-bgd base-color-inv" }, "Cancel"], // [0]=Y
 }
 
 export const swapPlayerAlert = {
-  message: "Are you sure you want to swap "+"these players"+"? The match(es) have already been reported.",
+  title: "Confirm Swap",
+  message: "One or both matches have already been reported.",
   // "these players" = (nameA ? nameA + ' w/ ' + nameB : 'these players')
   buttons: ["Swap","Cancel"], // [0]=Y
 }
 
+
+// --- Player Alerts --- \\
+
+export const deletePlayerAlert = (name) => ({
+  title: "Confirm Delete",
+  message: `All of ${name || defPlayer.toLowerCase()}'s info will be lost.`,
+  buttons: [{ value: "Delete", className: "neg-bgd base-color-inv" }, "Cancel"], // [0]=Y
+})
+
+export const cantDeletePlayerAlert = (name) => ({ title: "Can't Delete", message: name+" is registered for events." });
+
+export const duplicateNameAlert = (name) => ({ title: "Invalid Player", message: (name || defPlayer) + " is already there." })
+
+export const emptyNameAlert = { title: "Invalid Player", message: "They need a name." }
+
+
+// --- Settings Alerts --- \\
+
 export const resetDbAlert = {
-  title: "Erase ALL DATA?", message: "ALL players & games will be lost with no hope of recovery!",
+  title: "Confirm Erase", message: "ALL players & games will be lost with no hope of recovery!",
   buttons: [{ value: "Erase Database", className: "neg-bgd base-color-inv" }, "Cancel"], // [0]=Y
   className: "base-bgd max-color",
 }
 
 export const resetDbAlertConfirm = {
-  title: "Confirm Total Erase", message: "Are you sure you want to go through with this?",
+  title: "Erase ALL DATA", message: "Are you sure you want to go through with this?",
   buttons: ["Cancel", { value: "Erase Database", className: "neg-bgd base-color-inv" }], // [1]=Y
   className: "base-bgd max-color",
 }
+
 
 // --- Errors --- \\
 
