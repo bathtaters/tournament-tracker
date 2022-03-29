9c001cfa0f3167090b12c43fb31cab1844b51916
bathtaters <nomoremrnice7@gmail.com>
2022-03-29 14:37:02 -0400

    remove unnecessary player rankings updates

--- client/src/pages/event/services/eventFetch.services.js
+++ client/src/pages/event/services/eventFetch.services.js
@@ -1,30 +1,30 @@
-import { fetchApi } from '../../common/common.fetch';
+import { fetchApi, setFetch } from '../../common/common.fetch';
 import { fakeRound } from './event.services';
 
-export function nextRoundUpdate(id, { dispatch, getState }) {
-  const current = getState().dbApi.queries['event("'+id+'")'].data;
-  if (current.status < 2) dispatch(fetchApi.util.updateQueryData('stats', id, draft => {
-    draft.ranking = current.players;
-  }));
+export function nextRoundUpdate(id, { dispatch }) {
   dispatch(fetchApi.util.updateQueryData('event', id, draft => { 
-    if (draft.roundactive > draft.roundcount) return;
-    if (!draft.matches) draft.matches = [];
+    if (draft.roundactive > draft.roundcount) return; // Handle error
+    if (!draft.matches) draft.matches = []; // Handle no matches
+    if (draft.status < 2) draft.status = 2; // Handle initial round
+
+    // Add next round
     if (draft.roundactive++ < draft.roundcount)
       draft.matches.push(fakeRound(draft));
+    
+    // End draft + cancel update
     else draft.status = 3;
-    if (draft.status < 2) draft.status = 2;
   }));
 }
 
-export function clearRoundUpdate(id, { dispatch, getState }) {
-  const current = getState().dbApi.queries['event("'+id+'")'].data;
-  if (current.roundactive === 1) dispatch(fetchApi.util.updateQueryData('stats', id, draft => {
-    draft.ranking = [];
-  }));
+export function clearRoundUpdate(id, { dispatch }) {
   dispatch(fetchApi.util.updateQueryData('event', id, draft => { 
-    if (draft.roundactive < 1) return;
+    if (draft.roundactive < 1) return; // Handle error
+
+    // Remove round
     draft.matches.pop();
     draft.roundactive = draft.matches.length;
+
+    // Fix status on start/end event
     if (!draft.roundactive) draft.status = 1;
     else if (draft.status === 3) draft.status = 2;
   }));
