1206166c029161cad40ce51e0494d57bd71ea234
bathtaters <nomoremrnice7@gmail.com>
2022-03-30 00:24:30 -0400

    add event.notes

--- api/src/db/sql/resetDb.sql
+++ api/src/db/sql/resetDb.sql
@@ -41,6 +41,7 @@ CREATE TABLE event (
     roundcount SMALLINT NOT NULL DEFAULT 3,
     wincount SMALLINT NOT NULL DEFAULT 2,
     playerspermatch SMALLINT NOT NULL DEFAULT 2,
+    notes STRING NOT NULL DEFAULT '',
 
     -- Clock base (clocklimit set by user, others set by start/stop/etc)
     clocklimit INTERVAL NOT NULL DEFAULT '60 mins',
@@ -79,14 +80,14 @@ GRANT SELECT ON TABLE * TO db_read;
 
 CREATE VIEW eventDetail (
     id, title, players, playerspermatch,
-    day, slot, roundactive, roundcount, wincount,
+    day, slot, roundactive, roundcount, wincount, notes,
     allreported,
     anyreported,
     byes,
     drops
 ) AS SELECT
     event.id, event.title, event.players, playerspermatch,
-    day, slot, roundactive, roundcount, wincount,
+    day, slot, roundactive, roundcount, wincount, notes,
     BOOL_AND(reported),
     BOOL_OR(reported) FILTER(
         WHERE match.round = roundactive AND ARRAY_LENGTH(match.players, 1) != 1),
