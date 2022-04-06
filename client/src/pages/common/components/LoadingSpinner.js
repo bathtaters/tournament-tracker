3510ebf05e7da438f6699b8f8501d7ac1d2ed245
bathtaters <nomoremrnice7@gmail.com>
2022-04-06 12:20:44 -0400

    use built-in tailwind spinner animation

--- client/src/pages/common/components/LoadingSpinner.js
+++ client/src/pages/common/components/LoadingSpinner.js
@@ -4,7 +4,7 @@ import React from "react";
 function LoadingStyle({ size = 4, weight = 4, hideBgd = false, color = '#3498db', className = '' }) {
   return (
     <div style={{borderTopColor: color}} className={
-      'loader ease-linear rounded-full dimmer-border ' +
+      'animate-spin ease-linear rounded-full dimmer-border ' +
       `border-${hideBgd ? 0 : weight} border-t-${weight} ` +
       `h-${size} w-${size} sm:h-${size+2} sm:w-${size+2}` +
       className
