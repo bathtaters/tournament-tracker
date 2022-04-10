5e7e65d1b796bb6272a0a8703e518b2cef8b38dd
bathtaters <nomoremrnice7@gmail.com>
2022-04-10 18:44:17 -0400

    update for onRowHover (+ rmv semicolons)

--- client/src/pages/playerEvents/PlayerEvents.js
+++ client/src/pages/playerEvents/PlayerEvents.js
@@ -1,23 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "react"
+import PropTypes from "prop-types"
 
-import DataTable from "../common/DataTable";
-import Loading from "../common/Loading";
-import RawData from "../common/RawData";
-import { WrapperStyle } from "./styles/PlayerEventStyles";
+import DataTable from "../common/DataTable"
+import Loading from "../common/Loading"
+import RawData from "../common/RawData"
+import { WrapperStyle } from "./styles/PlayerEventStyles"
 
-import eventsLayout from "./playerEvents.layout";
-import { useEventQuery, usePlayerEventsQuery, usePlayerMatchesQuery, usePrefetchEvent } from "./playerEvents.fetch";
+import eventsLayout from "./playerEvents.layout"
+import { useEventQuery, usePlayerEventsQuery, usePlayerMatchesQuery, usePrefetchEvent } from "./playerEvents.fetch"
 
 
 function PlayerEvents({ id }) {
   // Fetch global state
-  const { data: eventIds, isLoading, error } = usePlayerEventsQuery(id);
-  const { data: matches, isLoading: matchLoad, error: matchErr } = usePlayerMatchesQuery(id);
-  const { data: events,  isLoading: eventLoad, error: eventErr } = useEventQuery();
+  const { data: eventIds, isLoading, error } = usePlayerEventsQuery(id)
+  const { data: matches, isLoading: matchLoad, error: matchErr } = usePlayerMatchesQuery(id)
+  const { data: events,  isLoading: eventLoad, error: eventErr } = useEventQuery()
 
   // Setup pre-fetching
-  const prefetch = usePrefetchEvent();
+  const prefetch = usePrefetchEvent()
 
   // Handle loading/errors
   const [ loading, err ] = [ isLoading || matchLoad || eventLoad,  error || matchErr || eventErr ];
@@ -40,7 +40,7 @@ function PlayerEvents({ id }) {
         className="mx-4"
         cellClass="py-1"
         hdrClass="dim-color text-left text-xl"
-        onHover={'TO DO:' || prefetch}
+        onRowHover={prefetch}
       >
         – None –
       </DataTable>
@@ -50,6 +50,6 @@ function PlayerEvents({ id }) {
   );
 }
 
-PlayerEvents.propTypes = { id: PropTypes.string };
+PlayerEvents.propTypes = { id: PropTypes.string }
 
-export default PlayerEvents;
+export default PlayerEvents
