8f7441ac8f7f45a28c26701d9642cec9dfc071b9
bathtaters <nomoremrnice7@gmail.com>
2022-04-11 19:59:17 -0400

    init commit, add escapedLength for string length

---
+++ api/src/utils/validate.utils.js
@@ -0,0 +1,49 @@
+const parseInterval = require('postgres-interval')
+
+// Decode validation types to [fullStr, typeStr, leaveWhiteSpace (*), isArray ([]), isOptional (?)]
+const typeRegex = /^([^[?*]+)(\*)?(\[\])?(\?)?$/
+exports.getTypeArray = (typeStr) => typeStr && typeStr.match(typeRegex)
+
+// Setup date-only parsing
+const strictDates = true
+exports.dateOptions = {
+  date: { format: 'YYYY-MM-DD', strict: strictDates, delimiters: ['-'] },
+  time: { strict: strictDates, strictSeparator: strictDates },
+}
+
+// Setup custom validator/sanitizer for intervals
+exports.customInterval = {
+  sanitize: { options: parseInterval },
+  validate: (value) => /^\d{2}(?::\d{2}){0,2}$/.test(value),
+}
+
+
+// Count escaped string (each escaped char counts as 1)
+const ESC_START = '&', ESC_END = ';', ESC_MAX_INT_LEN = 5 // Escaping params
+exports.escapedLength = ({ options, errorMessage }) => ({
+  errorMessage,
+  options: (value) => {
+    if (!options || !options.min && !options.max) return true
+    if (typeof value !== 'string') return false
+
+    let count = 0, isEsc = false, reserve
+    for (const c of value) {
+      if (c === ESC_START) {
+        if (reserve) count += reserve
+        isEsc = true
+        reserve = 0
+      } else if (isEsc) {
+        const isEnd = c === ESC_END
+        if (isEnd || reserve > ESC_MAX_INT_LEN) {
+          if (!isEnd) count += reserve
+          isEsc = false
+          reserve = 0
+        }
+      }
+      isEsc ? reserve++ : count++
+    }
+    if (reserve) count += reserve
+    console.debug(' #### check esc count: ',count,options)
+    return (!options.min || count >= options.min) && (!options.max || count <= options.max)
+  }
+})
