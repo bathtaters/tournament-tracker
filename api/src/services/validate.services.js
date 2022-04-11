fa5e99f2d7560433386c3bb65099ce24bfc5fe5e
bathtaters <nomoremrnice7@gmail.com>
2022-04-11 19:58:52 -0400

    centralize errorText, +move utils to validateUtils

--- api/src/services/validate.services.js
+++ api/src/services/validate.services.js
@@ -1,51 +1,51 @@
 // Import
 const logger = require('../utils/log.adapter')
 const validCfg = require('../config/validation')
-const parseInterval = require('postgres-interval')
+const { getTypeArray, dateOptions, customInterval, escapedLength } = require('../utils/validate.utils')
 
-// Get Error Messages
-const errMsg = (type) => 
-  `does not exist as ${type}`
-const limitErr = ({ min, max }, isStr = false) => 
-  `is too ${isStr ? 'long/short' : 'large/small'} (must be between ${min} & ${max}${isStr ? ' characters' : ''})`
-const missingErr = (key, type) => 
-  `${key} has ${type ? 'invalid' : 'missing'} type definition: ${type || ''}`
-const missingInErr = (key) =>
-  `${key} missing 'in' array for validation`
+// Error Messages
+const errorText = {
+  // Static
+  exists:   'must be included',
+  uuid:     'not a valid UUID',
+  string:   'not a valid string',
+  float:    'not a valid decimal',
+  int:      'not a valid number',
+  boolean:  'not a valid boolean',
+  interval: 'not a valid interval',
+  datetime: 'not a valid timestamp',
+  date:     'not a valid date',
+  object:   'not a valid object',
+  // Variable
+  type:      (type)      => `does not exist as ${type}`,
+  missing:   (key, type) => `${key} has ${type ? 'invalid' : 'missing'} type definition: ${type || ''}`,
+  missingIn: (key)       => `${key} missing 'in' array for validation`,
+  limit:     ({ min, max }, isStr = false) => 
+    `is too ${isStr ? 'long/short' : 'large/small'} (must be between ${min} & ${max}${isStr ? ' characters' : ''})`,
+}
 
-// Decode validation types to [fullStr, typeStr, leaveWhiteSpace (*), isArray ([]), isOptional (?)]
-const typeRegex = /^([^[?*]+)(\*)?(\[\])?(\?)?$/
 
-// Setup date-only parsing
-const strictDates = true
-const dateOptions = { format: 'YYYY-MM-DD', strict: strictDates, delimiters: ['-'] }
-
-// Setup custom validator/sanitizer for intervals
-const validInterval = { options: (value) => /^\d{2}(?::\d{2}){0,2}$/.test(value), errorMessage: 'not a valid interval' }
-const sanitInterval = { options: parseInterval }
-
-// Main
-
+// Generate Schema object based on input
 function getSchema(key, typeStr, limits, isIn, forceOptional = false) {
-  if (!isIn || !isIn.length) throw new Error(missingInErr(key))
+  if (!isIn || !isIn.length) throw new Error(errorText.missingIn(key))
 
   // Get type from typeStr
-  const type = (typeStr || '').match(typeRegex)
-  if (!type || !type[0]) throw new Error(missingErr(key, typeStr))
+  const type = getTypeArray(typeStr)
+  if (!type || !type[0]) throw new Error(errorText.missing(key, typeStr))
   if (forceOptional) type[4] = '?'
   if (type[2] && type[1] !== 'string') logger.warn('* is ignored w/ non-string type: ', type[0])
 
   // Initialize ptr & static values (errMsg/in)
   let valid = { [key]: {} }
   let ptr = valid[key]
-  ptr.errorMessage = errMsg(type[0])
+  ptr.errorMessage = errorText.type(type[0])
   ptr.in = isIn
 
   // Add validation for optionals/non-optionals
   if (type[4]) {
     ptr.optional = { options: { nullable: true, checkFalsy: type[1] === 'string' } }
   } else {
-    ptr.exists = { errorMessage: 'must be included' }
+    ptr.exists = { errorMessage: errorText.exists }
     
     // Skip validation of empty strings (only if empty strings are allowed)
     if (type[1] === 'string' && limits && (limits.elem || limits) && (limits.elem || limits).min === 0) {
@@ -64,58 +64,58 @@ function getSchema(key, typeStr, limits, isIn, forceOptional = false) {
       arrLimit = limits
       limits = null
     }
-    ptr.isArray = arrLimit ? { options: arrLimit, errorMessage: limitErr(arrLimit, false) } : { errorMessage: errMsg('array') }
+    ptr.isArray = { options: arrLimit, errorMessage: errorText.limit(arrLimit || 'array') }
     
     // Create entry & update ptr
     valid[key+'.*'] = {}
     ptr = valid[key+'.*']
 
     // Set statics for new entry
-    ptr.errorMessage = errMsg(type[1])
+    ptr.errorMessage = errorText.type(type[1])
     ptr.in = isIn
   }
 
   // Pass limits as options
   if (limits && (limits.array || limits.elem)) limits = limits.elem
-  if (limits) limits = { options: limits, errorMessage: limitErr(limits, type[1] === 'string') }
+  if (limits) limits = { options: limits, errorMessage: errorText.limit(limits, type[1] === 'string') }
 
   // Set type-specific validators/sanitizers
   switch (type[1]) {
-    case 'uuid': ptr.isUUID = { options: 4, errorMessage: 'not a valid UUID' } // pass to string
+    case 'uuid': ptr.isUUID = { options: 4, errorMessage: errorText.uuid } // pass to string
     case 'string':
-      ptr.isString = { errorMessage: 'not a valid string' }
+      ptr.isString = { errorMessage: errorText.string }
       if (!type[2]) { 
         ptr.stripLow = true
         ptr.trim = true
       }
       ptr.escape = true
-      if (limits) ptr.isLength = limits
+      if (limits) ptr.custom = escapedLength(limits)
       break
     case 'float':
-      ptr.isFloat = limits || { errorMessage: 'not a valid decimal' }
+      ptr.isFloat = limits || { errorMessage: errorText.float }
       ptr.toFloat = true
       break
     case 'int':
-      ptr.isInt = limits || { errorMessage: 'not a valid number' }
+      ptr.isInt = limits || { errorMessage: errorText.int }
       ptr.toInt = true
       break
     case 'boolean':
-      ptr.isBoolean = { errorMessage: 'not a valid bool' }
+      ptr.isBoolean = { errorMessage: errorText.boolean }
       ptr.toBoolean = true
       break
     case 'interval':
-      ptr.custom = validInterval
-      ptr.customSanitizer = sanitInterval
+      ptr.custom = { options: customInterval.validate, errorMessage: errorText.interval },
+      ptr.customSanitizer = customInterval.sanitize
       break
     case 'datetime':
-      ptr.isISO8601 = { options: { strict: strictDates, strictSeparator: strictDates }, errorMessage: 'not a valid timestamp' }
+      ptr.isISO8601 = { options: dateOptions.time, errorMessage: errorText.datetime }
       ptr.toDate = true
       break
     case 'date':
-      ptr.isDate = { options: dateOptions, errorMessage: 'not a valid date' }
+      ptr.isDate = { options: dateOptions.date, errorMessage: errorText.date }
       ptr.trim = true
       break
-    case 'object': ptr.isObject = { errorMessage: 'not a valid object' } // pass to default
+    case 'object': ptr.isObject = { errorMessage: errorText.object } // pass to default
     case 'any':  // pass to default
     default: break
   }
@@ -123,6 +123,7 @@ function getSchema(key, typeStr, limits, isIn, forceOptional = false) {
   return valid
 }
 
+// Generate schema object based on ValidCfg file
 function getSchemaFromCfg(set, key, isIn = ['params'], optionalIfOnlyBody = false) {
   // Determine if optional flag should be forced
   let forceOptional = false
