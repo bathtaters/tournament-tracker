306d78e29773fa4c469cbdea41e00d73bf33aa68
bathtaters <nomoremrnice7@gmail.com>
2022-04-20 18:36:51 -0400

    badge eventEditor status

--- client/src/pages/eventEditor/styles/EventEditorStyles.js
+++ client/src/pages/eventEditor/styles/EventEditorStyles.js
@@ -10,7 +10,7 @@ export function StatusStyle({ status }) {
   return (
     <h5 className="text-center mb-2">
       <span className="mr-1">Status:</span>
-      <span className={"font-thin "+statusInfo[status].class}>{statusInfo[status].label}</span>
+      <span className={"badge badge-lg "+statusInfo[status].badge}>{statusInfo[status].label}</span>
     </h5>
   );
 }
