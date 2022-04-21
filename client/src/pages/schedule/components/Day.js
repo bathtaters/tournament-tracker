38b4ade1ec954f55ebdcbf556b8687aa7d20f5d5
bathtaters <nomoremrnice7@gmail.com>
2022-04-21 00:01:44 -0400

    fix missing border color style

--- client/src/pages/schedule/components/Day.js
+++ client/src/pages/schedule/components/Day.js
@@ -25,7 +25,7 @@ function Day({ events, isEditing, isSlotted, setEventModal, day, eventData }) {
       onDrop={dropHandler}
       canDrop={canDrop}
       className={dragAndDropClass.outer}
-      borderClass={{disabledColor: borderCls.slice(7)}}
+      borderClass={{disabledColor: borderCls}}
       dataType={dataType}
       disabled={(isSlotted && day !== noDate) || !isEditing}
       draggable={false}
