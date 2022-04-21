efe4ff6b0ba7709594b79b96b935aeac030cbe6f
bathtaters <nomoremrnice7@gmail.com>
2022-04-21 01:28:49 -0400

    upgrade to React v18 + Redux v8

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import store from './core/store/store';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
