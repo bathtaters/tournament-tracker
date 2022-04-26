a151679600aaf2ebced2d5a8383f5213ae096293
bathtaters <nomoremrnice7@gmail.com>
2022-04-26 14:10:05 -0400

    move ErrorBoundary to App / fix initial load

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -1,8 +1,7 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import ErrorBoundary from "./pages/common/ErrorBoundary";
-
+import Loading from "./pages/common/Loading";
 import { usePrefetchBase } from "./pages/common/common.hooks";
 
 // Lazy load each main route
@@ -15,18 +14,16 @@ function Routing() {
   usePrefetchBase(); // Preload page data
 
   return (
-    <ErrorBoundary>
-      <Suspense fallback={<h4 className="m-2 text-center">Loading your data...</h4>}>
-        <Routes>
-          <Route path="/"            element={<Navigate replace to="/home" />} />
-          <Route path="/home"        element={<Schedule />} />
-          <Route path="/event/:id"   element={<Event    />} />
-          <Route path="/players"     element={<Players  />} />
-          <Route path="/profile/:id" element={<Profile  />} />
-          <Route path="*"            element={<Navigate replace to="/home" />} />
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense fallback={<Loading loading={true} />}>
+      <Routes>
+        <Route path="/"            element={<Navigate replace to="/home" />} />
+        <Route path="/home"        element={<Schedule />} />
+        <Route path="/event/:id"   element={<Event    />} />
+        <Route path="/players"     element={<Players  />} />
+        <Route path="/profile/:id" element={<Profile  />} />
+        <Route path="*"            element={<Navigate replace to="/home" />} />
+      </Routes>
+    </Suspense>
   );
 }
 
