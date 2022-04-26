44e0d39faae3dc18b7168ea6f1635731e51382b9
bathtaters <nomoremrnice7@gmail.com>
2022-04-26 15:46:13 -0400

    use reload icon and use button tag

--- client/src/pages/header/components/ReloadButton.js
+++ client/src/pages/header/components/ReloadButton.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import { reloadClass } from "../styles/HeaderStyles";
+import ReloadIcon from "../../common/icons/ReloadIcon";
+import { reloadingClass } from "../styles/HeaderStyles";
 import { useForceRefetch, useFetchingProvider } from "../../common/common.fetch";
 
 function ReloadButton(props) {
   // Check for active queries
-  const isFetching = useFetchingProvider(); // setup isFetching global
+  const _isFetching = useFetchingProvider(); // setup isFetching global
+  const isFetching = props.force ?? _isFetching
 
   // Force refetch of all data
   const forceRefetch = useForceRefetch();
 
   // Generate class
-  const buttonClass = `${props.force ?? isFetching ? reloadClass.loading : reloadClass.button} ${props.className ?? ''}`
+  const buttonClass = `${isFetching ? reloadingClass : ''} ${props.className ?? ''}`
 
-  return <h4 className={buttonClass} onClick={forceRefetch}>↻</h4>
+  return <button type="button" className={buttonClass} disabled={isFetching} onClick={forceRefetch}><ReloadIcon /></button>
 }
 
 ReloadButton.propTypes = { 
