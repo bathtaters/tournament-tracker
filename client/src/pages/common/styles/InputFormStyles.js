34351478ff61e4fb1a3989daacdfebbb511c6993
bathtaters <nomoremrnice7@gmail.com>
2022-04-27 05:56:59 -0400

    improve default styling, + FormError style

--- client/src/pages/common/styles/InputFormStyles.js
+++ client/src/pages/common/styles/InputFormStyles.js
@@ -1,12 +1,22 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React from "react"
+import PropTypes from 'prop-types'
+
+import LockIcon from "../icons/LockIcon"
+import WarningIcon from "../icons/WarningIcon"
 
 // Input Element
 export const elementDefaults = {
   className:  "font-light mx-1 sm:mx-4",
   labelClass: "whitespace-nowrap",
+  inputWrapperClass: "flex",
   inputClass: "",
   buttonClass: "btn-primary mx-1 sm:mx-4",
+}
+export const typeDefaults = {
+  text: 'input input-bordered w-full invalid:input-warning',
+  date: 'input input-bordered w-full invalid:input-warning',
+  number: 'input input-bordered w-full invalid:input-warning p-1 text-center hide-arrows',
+  numberSize: 'w-12 sm:w-16'
 }
 
 // Form wrapper
@@ -14,10 +24,22 @@ export function FormContainer({ onSubmit, children }) {
   return (<form onSubmit={onSubmit}>{children}</form>)
 }
 
+// Error Banner
+export function FormErrorStyle({ children }) {
+  return (
+    <div className="alert alert-warning shadow-lg">
+      <div>
+        <WarningIcon className="stroke-current flex-shrink-0 h-6 w-6" />
+        <span>{children}</span>
+      </div>
+    </div>
+  )
+}
+
 // Row/Column wrapper
 export function RowStyle({ isRow, children }) {
   return (
-    <div className={`m-1 flex justify-start items-baseline flex-${isRow ? "row flex-wrap" : "col"}`}>
+    <div className={`m-1 flex justify-start ${isRow ? "flex-row flex-wrap" : "flex-col"}`}>
       {children}
     </div>
   );
@@ -31,11 +53,11 @@ export function ElementStyle({ isFragment, isLabel, className, children }) {
 }
 
 // Apply Disabled Lock to Input Elements
-export function InputStyle({ disabled, children }) {
+export function InputStyle({ disabled, className, children }) {
   return (
-    <label className={disabled ? "input-group" : ""}>
+    <label className={(disabled ? "input-group " : "") + className}>
       { children }
-      { disabled && <span className="bg-base-200">🔒</span> }
+      { disabled && <span className="bg-base-200 text-secondary"><LockIcon /></span> }
     </label>
   )
 }
