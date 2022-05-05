4e2f33e77ce0242912a7ac5d610610e91cb0c11c
bathtaters <nomoremrnice7@gmail.com>
2022-05-05 14:17:46 -0400

    fix top of modal under address bar on mobile

--- client/src/pages/common/styles/ModalStyle.js
+++ client/src/pages/common/styles/ModalStyle.js
@@ -3,8 +3,11 @@ import React from "react";
 // Modal window style (fwdRef for FocusTrap)
 export const ModalStyle = React.forwardRef(function ModalStyle({ className, onClick, z = 'z-70', children }, ref) {
   return (
-    <div className={`modal modal-open modal-bottom sm:modal-middle ${z}`} onClick={onClick} ref={ref}>
-      <div className={`modal-box sm:overflow-x-hidden bg-base-300 relative p-8 max-h-screen ${className}`} onClick={(e) => e.stopPropagation()}>
+    <div className={`modal modal-open modal-bottom sm:modal-middle h-full w-full ${z}`} onClick={onClick} ref={ref}>
+      <div
+        className={`modal-box max-h-full max-w-full sm:overflow-x-hidden bg-base-300 relative p-8 ${className}`}
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
@@ -18,6 +21,6 @@ export function CloseButton({ onClick }) {
     value="✕"
     aria-label="Close"
     onClick={onClick}
-    className="btn btn-ghost btn-circle btn-sm focus-visible:outline-none focus-visible:bg-neutral-500/30 absolute top-0 right-0"
+    className="btn btn-ghost btn-circle btn-sm focus-visible:outline-none focus-visible:bg-base-content/20 absolute top-0 right-0"
     />);
 }
