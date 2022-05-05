3c6d00ac2eb7a00ba732e3c4cd51b8729209314e
bathtaters <nomoremrnice7@gmail.com>
2022-05-05 14:16:27 -0400

    move style classes to styles file

--- client/src/pages/common/components/InputForm/NumberPicker.js
+++ client/src/pages/common/components/InputForm/NumberPicker.js
@@ -7,7 +7,7 @@ export default function NumberPicker({ inputProps, backend, className, wrapperCl
   return (
     <div className={"input-group "+wrapperClass}>
       <button type="button" data-action="decrement" className="btn btn-ghost btn-sm sm:btn-md" onClick={decHandler}>－</button>
-      <input {...inputProps} className={className+' h-8 sm:h-12'} onInvalid={invalidHandler} pattern="\\d*" />
+      <input {...inputProps} className={className} onInvalid={invalidHandler} pattern="\\d*" />
       <button type="button" data-action="increment" className="btn btn-ghost btn-sm sm:btn-md" onClick={incHandler}>＋</button>
     </div>
   )
