6c394d4e4888a4fd68c86b334794385b4f3a4c4c
bathtaters <nomoremrnice7@gmail.com>
2022-09-24 17:51:29 -0400

    remove repeated services

--- client/src/pages/common/SuggestText/services/suggestText.utils.js
+++ client/src/pages/common/SuggestText/services/suggestText.utils.js
@@ -1,4 +1,8 @@
-import { useEffect, useCallback, useState, useLayoutEffect } from "react"
+import { useLayoutEffect } from "react"
+
+// Must include when porting SuggestText
+export { useScrollToRef, useHotkeys } from '../../services/basic.services'
+
 
 // Determine if list is valid
 export const validList = (list) => Array.isArray(list) && Boolean(list.length);
@@ -37,63 +41,4 @@ export function useLayoutListener(listenerTypes, effect, deps) {
     return () => { listenerTypes.forEach((type) => window.removeEventListener(type, effect)) }
   // eslint-disable-next-line
   }, deps)
-}
-
-
-
-// Provides a ref to give to an object you want to be scrolled to if invisible
-//    options = see IntersectionObserver options & ScrollIntoView options
-export function useScrollToRef({
-  // Observer options
-  rootRef = null, threshold = 1.0, rootMargin = "0px",
-  // ScrollIntoView options
-  behavior = "auto", block = "start", inline = "nearest", 
-} = {}) {
-  // Create scroll callback
-  const [ elementRef, setRef ] = useState(null)
-
-  // Add/Remove observer listener
-  useEffect(() => {
-    const root = rootRef && 'current' in rootRef ? rootRef.current : rootRef
-
-    const observer = new IntersectionObserver((entries) => {
-      if (elementRef && !entries[0].isIntersection)
-        elementRef.scrollIntoView({ behavior, block, inline })
-
-      }, {root, threshold, rootMargin})
-
-    if (elementRef) observer.observe(elementRef)
-
-    return () => { if (elementRef) observer.unobserve(elementRef) }
-  }, [elementRef, rootRef, threshold, rootMargin, behavior, block, inline])
-
-  // Get reference to element
-  return (newRef) => setRef(newRef)
-}
-
-
-
-// Listen & handle hotkeys
-// hotkeyMap = { [keyCode]: () => action(), ... }
-export function useHotkeys(hotkeyMap, { skip, deps } = {}) {
-  const hotkeyHandler = useCallback((ev) => {
-    // console.debug(' >> KeyName: ',ev.key); // print names of keys
-
-    if (!hotkeyMap[ev.key]) return;
-    ev.preventDefault();
-
-    if (typeof hotkeyMap[ev.key] === 'function') hotkeyMap[ev.key](ev);
-    else console.error('Malformed keyMap for', ev.key, hotkeyMap[ev.key]);
-  
-  // eslint-disable-next-line
-  }, deps);
-
-  useEffect(() => {
-    if (!skip) document.addEventListener('keydown', hotkeyHandler, false);
-    else document.removeEventListener('keydown', hotkeyHandler, false);
-
-    return () => document.removeEventListener('keydown', hotkeyHandler, false);
-  
-  // eslint-disable-next-line
-  }, deps && deps.concat(skip));
 }
