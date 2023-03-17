f93b9d69578e06de9f01b94e59a9316301e106d7
Nick C <bathtaters@gmail.com>
2023-03-16 21:09:56 -0400

    fix build error and add output

--- deploy.example.sh
+++ deploy.example.sh
@@ -23,27 +23,38 @@ cd "$repopath"
 git pull
 
 # Update API
+echo
+echo " *** UPDATING API *** "
 cd "$repopath/api"
 npm install
 npm update
 
 # Update Client
+echo
+echo " *** UPDATING CLIENT *** "
 cd "$repopath/client"
 npm install
 npm update
-npm build
+npm run build
 
 # Deploy Client
+echo
+echo " *** DEPLOYING CLIENT *** "
 if [ -d "$livepath" ]; then
     mv -f "$livepath" "$livepath-old"
-    echo "Old client backed up: $livepath-old"
+    echo "> Old client backed up: $livepath-old"
 else
-    echo "Old client not found"
+    echo "> Old client not found"
 fi
 
-cp -a "$repopath/client/build" "$livepath" && echo "New build is live."
+cp -a "$repopath/client/build" "$livepath" && echo "> New client build is live."
 
 # Deploy API
+echo
+echo " *** DEPLOYING API *** "
 pm2 stop "$apiname"
 pm2 start "$apiname"
 pm2 logs "$apiname"
+
+echo
+echo " *** UPDATED AND DEPLOYED SUCCESSFULLY *** "
