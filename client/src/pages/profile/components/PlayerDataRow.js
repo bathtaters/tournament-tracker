07a699808723078cb1da76d5a6188ca4a7f0374a
Nick C <bathtaters@gmail.com>
2023-08-31 23:06:59 -0400

    check name uniqueness & move all logic to hook

--- client/src/pages/profile/components/PlayerDataRow.js
+++ client/src/pages/profile/components/PlayerDataRow.js
@@ -1,31 +1,13 @@
-import React, { useCallback, useState } from "react";
-
+import React from "react";
 import InputBox from "./InputBox";
 import { EditButton, LabelStyle, InputGroupStyle } from "../styles/PlayerDataStyles";
-
-import { useUpdatePlayerMutation } from "../profile.fetch";
-import { editClickController, saveController } from "../services/profile.services";
-import { useHotkeys } from "../../common/services/basic.services";
+import usePlayerData from "../services/profile.services";
 import { READ, WRITE } from "../profile.layout";
 
 
 function PlayerDataRow({ rowData, data, id, access }) {
-  if (data == null) data = '';
 
-  // Global state
-  const [ updatePlayer ] = useUpdatePlayerMutation();
-  
-  // Local state
-  const [isEditing, setEditing] = useState(access === WRITE);
-  const [editData, setEditData] = useState(data);
-  
-  // Actions
-  const changeData = (e) => setEditData(e.target.value);
-  const saveData = saveController(id, rowData, editData, data, updatePlayer);
-  const setEdit = useCallback((edit = true) => { edit && setEditData(data); setEditing(edit); }, [data]);
-  const handleClick = editClickController(isEditing, saveData, setEdit);
-
-  useHotkeys({ Enter: handleClick }, { skip: !isEditing });
+  const { editData, isEditing, handleClick, changeData, setEdit } = usePlayerData(rowData, data, access, id);
 
   if (!access) return;
 
--- client/src/pages/profile/services/profile.services.js
+++ client/src/pages/profile/services/profile.services.js
@@ -1,20 +1,51 @@
+import { useCallback, useState } from "react";
+import { useUpdatePlayerMutation, usePlayerState } from "../profile.fetch";
+import { useHotkeys } from "../../common/services/basic.services";
+import { useOpenAlert } from "../../common/common.hooks";
 import { fetchApi } from '../../common/common.fetch';
+import { WRITE } from "../profile.layout";
+import { duplicateNameAlert } from "../../../assets/alerts";
 
-// Profile controllers
-export function editClickController(isEditing, saveData, setEdit) {
-  // Begin edit
-  if (!isEditing) return () => setEdit(true);
-  // Save edit
-  return () => {
-    saveData();
-    setEdit(false);
-  }
-}
 
-export const saveController = (id, { id: key, required }, value, data, updateData) => () => {
-  if (required && (!value || !value.trim())) return;
-  if (value && value.trim() === data) return;
-  return updateData({ [key]: value || null, id });
+// Profile Controller
+export default function usePlayerData(rowData, data, access, id) {
+  if (data == null) data = '';
+
+  // Global state
+  const { data: allPlayers } = usePlayerState();
+  const [ updatePlayer ] = useUpdatePlayerMutation();
+  const openAlert = useOpenAlert();
+  
+  // Local state
+  const [isEditing, setEditing] = useState(access === WRITE);
+  const [editData, setEditData] = useState(data);
+  
+  // Form Control
+  const changeData = (e) => setEditData(e.target.value);
+
+  // Form Submit
+  const saveData = () => {  
+    const body = getUpdateBody(editData, data, rowData, id);
+    if (!body) return;
+
+    if (playerIsUnique(body, allPlayers)) return updatePlayer(body);
+    return openAlert(duplicateNameAlert(editData));
+  };
+
+  // Enable/Disable editing
+  const setEdit = useCallback((edit = true) => {
+    edit && setEditData(data);
+    setEditing(edit);
+  }, [data]);
+
+  const handleClick = !isEditing ?
+    () => setEdit(true) :
+    () => { saveData(); setEdit(false); };
+
+  useHotkeys({ Enter: handleClick }, { skip: !isEditing });
+
+
+  return { editData, isEditing, handleClick, changeData, setEdit };
 }
 
 
@@ -28,3 +59,24 @@ export function playerUpdate({ id, ...body }, { dispatch, queryFulfilled }) {
   }));
   queryFulfilled.catch(() => { updateAll.undo(); updateOne.undo(); }); // rollback
 };
+
+
+
+// Controller Utilities
+
+function getUpdateBody(editData, data, rowData, id) {
+  if (rowData.required && (!editData || !editData.trim())) return;
+  if (editData && editData.trim() === data) return;
+  
+  return { [rowData.id]: editData || null, id };
+}
+
+
+function playerIsUnique({ id, name }, players) {
+  if (!name || !players) return true;
+
+  const newName = name.trim().toLowerCase();
+  return Object.values(players).every(
+    (p) => id === p.id || newName !== p.name.toLowerCase()
+  );
+}
