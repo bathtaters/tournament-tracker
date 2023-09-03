c5f583a00c21329208e850389ef2c9565664d47e
Nick C <bathtaters@gmail.com>
2023-09-03 00:00:03 -0400

    make page wrapper stretch to fill page

--- client/src/pages/common/styles/CommonStyles.js
+++ client/src/pages/common/styles/CommonStyles.js
@@ -5,7 +5,7 @@ export function AppWrapperStyle({ children }) {
 }
 
 export function PageWrapperStyle({ children }) {
-  return (<div className="p-4 max-w-3xl flex-grow">{children}</div>);
+  return (<div className="p-4 max-w-3xl flex-grow w-full">{children}</div>);
 }
 
 export function PageTitleStyle({ className = "", children }) {
