f8c703cdd0fea0425c8d6c0809e1a3c8e4a7dd0e
Nick C <bathtaters@gmail.com>
2023-09-03 00:05:29 -0400

    fix voter routes

--- api/src/routes/voter.routes.js
+++ api/src/routes/voter.routes.js
@@ -7,13 +7,13 @@ const controller = require('../controllers/voter.controllers');
 // *** Player API commands *** \\
 
 // Gets
-router.get('/player/all',                           catcher(controller.getAllVotes));
-router.get('/player/:id',    validate.playerid,     catcher(controller.getVote));
+router.get('/all',                           catcher(controller.getAllVotes));
+router.get('/:id',    validate.playerid,     catcher(controller.getVote));
 
 // Sets
-router.post(  '/player/',    validate.newPlayer,    catcher(controller.addPlayer));
-router.delete('/player/:id', validate.playerid,     catcher(controller.removePlayer));
-router.patch( '/player/:id', validate.updatePlayer, catcher(controller.updateVote));
+router.post(  '/',    validate.newPlayer,    catcher(controller.addPlayer));
+router.delete('/:id', validate.playerid,     catcher(controller.removePlayer));
+router.patch( '/:id', validate.updatePlayer, catcher(controller.updateVote));
 
 
 module.exports = router;
