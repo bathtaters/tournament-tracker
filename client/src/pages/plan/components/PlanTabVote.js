b33d8b1c13d872b1716faaa0cfaedaf48d7b2890
Nick C <bathtaters@gmail.com>
2023-09-05 23:15:03 -0400

    connect voter.days to backend

--- client/src/pages/plan/components/PlanTabVote.js
+++ client/src/pages/plan/components/PlanTabVote.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react"
+import React from "react"
+import { useUpdateVoterMutation } from "../voter.fetch"
 import { PlanRowStyle } from "../styles/PlanStyles"
 import DateMultiSelect from "./DateMultiSelect"
 import { GeneralSectionStyle } from "../styles/PlanTabVoteStyles"
 import EventDragger from "./EventDragger"
 import { useRankState } from "../services/planVote.services"
 import { planEvents } from "../services/plan.utils"
+import { useServerListValue } from "../../common/common.hooks"
+import { plan as config } from "../../../assets/config"
 
 
 function PlanTabVote({ voter, events, settings }) {
     
+    const [ updateVoter ] = useUpdateVoterMutation()
+    
     const eventList = planEvents(events)
-    
-    const [ dates, setDates ] = useState([])
+
+    // Voter.Days
+    const [ dates, setDates ] = useServerListValue(
+        voter?.days,
+        (days) => updateVoter({ id: voter?.id, days }),
+        { throttleDelay: config.updateDelay },
+    )
+
+    // Voter.Events
     const [ ranked, setRanked ] = useRankState(eventList, voter)
-    
+
     const unranked = eventList.filter((id) => !ranked.includes(id))
     
 
