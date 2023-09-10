52864c7f4e82167bad0eb86150c595915943e4ca
Nick C <bathtaters@gmail.com>
2023-09-10 01:08:37 -0400

    update loading style to animation instead of text

--- client/src/pages/common/styles/LoadingStyle.js
+++ client/src/pages/common/styles/LoadingStyle.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from "react"
 
-export default function LoadingStyle({ TagName, className, children }){
-  return (
+export const LoadingWrapper = ({ children }) => (
+  <div className="flex flex-col justify-center items-center gap-4 m-4">
+      <div className="loading loading-dots loading-sm sm:loading-lg" />
+      <div className="font-light">{children}</div>
+  </div>
+)
+
+export const LoadingStyle = ({ TagName, className, children }) => (
     <TagName className={"italic text-center font-thin "+className}>
       {children}
     </TagName>
-  );
-}
+)
