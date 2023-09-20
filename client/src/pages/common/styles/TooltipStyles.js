73b631a9b72aaeb8fdb5f2dbdcf274d96d99fbfa
Nick C <bathtaters@gmail.com>
2023-09-20 15:31:10 -0400

    fix player hover z-index

--- client/src/pages/common/styles/TooltipStyles.js
+++ client/src/pages/common/styles/TooltipStyles.js
@@ -4,7 +4,7 @@ export const TooltipWrapperStyle = (props) => <div {...props} className={`relati
 
 export const TooltipStyle = (props) => (
     <div  {...props} className={
-        `absolute top-full left-[5%] right-[5%] mt-2 p-2 bg-base-300 rounded-lg
+        `absolute top-full left-[5%] right-[5%] z-10 mt-2 p-2 bg-base-300 rounded-lg
         opacity-0 group-hover:opacity-100 transition-opacity duration-500 ${props.className || ""}
         after:content-[""] after:absolute after:w-0 after:h-0
         after:top-0 after:left-1/2 after:mt-[-10px] after:ml-[-10px]
