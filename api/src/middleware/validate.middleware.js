7acc90fd3b9da685fca94b3c4b017e8c8b94192a
Nick C <bathtaters@gmail.com>
2024-08-24 22:48:05 -0400

    don't expose validation values to client

--- api/src/middleware/validate.middleware.js
+++ api/src/middleware/validate.middleware.js
@@ -1,13 +1,14 @@
 const { validationResult } = require('express-validator')
 
-const errorFormatter = ({ path, msg, value }) =>  `${path} ${msg}: ${JSON.stringify(value)}`
+const simpleFormat = ({ path, msg }) =>  `${path} ${msg}`
+const extendFormat = ({ path, msg, value }) =>  `${path} ${msg}: ${JSON.stringify(value)}`
 
 // Test validation middleware (Run after running other validators)
 const checkValidation = (req, _, next) => {
   const validErrors = validationResult(req)
   if (!validErrors.isEmpty())
     next({ 
-      message: validErrors.formatWith(errorFormatter).array().join(', '),
+      message: validErrors.formatWith(simpleFormat).array().join(', '),
       stack:
         '\n  Request data:' +
         '\n    URL: ' + req.originalUrl +
@@ -15,7 +16,7 @@ const checkValidation = (req, _, next) => {
         '\n    Params: ' + JSON.stringify(req.params) +
         '\n    Body: ' + JSON.stringify(req.body) +
         '\n  Validation errors:' +
-        '\n    ' + validErrors.formatWith(errorFormatter).array().join('\n    '),
+        '\n    ' + validErrors.formatWith(extendFormat).array().join('\n    '),
       status: 400,
     })
   next()
