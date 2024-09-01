aea34afcba7b098b4f423380cb003628451af316
Nick C <bathtaters@gmail.com>
2024-09-01 18:09:08 -0400

    add faster getCombinations methods

--- api/src/utils/combination.utils.js
+++ api/src/utils/combination.utils.js
@@ -1,5 +1,33 @@
 /** Calculate count of all possible combinations of array items in slotCount slots */
-const combinationCount = (arrayLen, slotCount) => slotCount && fact(arrayLen) / (fact(slotCount) * fact(arrayLen - slotCount))
+const combinationCount = (arrayLen, slotCount) => slotCount && Math.round(fact(arrayLen) / (fact(slotCount) * fact(arrayLen - slotCount)))
+
+
+/**
+ * Test if any combination of indexes does not include a match.
+ * @param {number[]} indexes - List of indexes to check
+ * @param {number[][]} matches - Array containing matching indexes for each index
+ * @returns {bool} - True if all indexes are matches
+ */
+const isUnique = (indexes, matches) => {
+    for (let a = 0; a < indexes.length; a++) {
+        for (let b = a + 1; b < indexes.length; b++) {
+            if (!matches[indexes[a]].includes(indexes[b])) return false
+        }
+    }
+    return true
+  }
+  
+  /**
+  * Generate a matches array for isUnique, checks for duplicate base-level items before approving a match
+  * @param {any[][]} array - 2D Array to check for matching inner elements
+  * @returns {number[][]} - input array indexes that for which the index they are placed at should match
+  */
+  const generateMatches = (array) => array.map(
+      (base) => array.map((_,i) => i).filter(
+          (i) => array[i].every((item) => !base.includes(item))
+      )
+  )
+
 
 /** Get the Nth combination of array items in slotCount slots
  *   No empties, order doesn't matter, comboCount = result of combinationCount() */
@@ -18,6 +46,79 @@ function getCombinationN(n, array, slotCount, comboCount) {
     }
     return combo
 }
+
+
+/**
+ * Generator that yields all unique combinations of array items of the given size.
+ *   (No empties, order doesn't matter, combinations will not contain duplicate items)
+ * @param {any[][]} array - A 2D array containing base-level items that can be tested for equality.
+ * @param {number} size - Size of each combination to return
+ * @returns {any[][]} - Next unique array of size 'size'.
+ */
+function* getUniqueCombinations(array, size) {
+    // Handle invalid array length
+    if (!array?.length) return
+
+    // Initialize pointers for each slot (Most active ptr first)
+    const slotPtr = Array(size).fill(0).map((_,i) => size - i - 1)
+    // Set limits for each pointer
+    const limit = slotPtr.map((_,i) => array.length - i - 1)
+  
+    // Create a map of indexes that match the given index
+    const matches = generateMatches(array)
+    
+    slotPtr[0]-- // First iteration fix
+    while (true) {
+        // Find first pointer that doesn't need to wrap
+        let wrapSlot = slotPtr.findIndex((ptr, idx) => ptr < limit[idx])
+        if (wrapSlot === -1) break // No more pointers left to wrap
+  
+        // Wrap all upstream pointers
+        while (wrapSlot--) slotPtr[wrapSlot] = ++slotPtr[wrapSlot + 1]
+  
+        // Base increment
+        ++slotPtr[0]
+  
+        // Convert pointers to actual array values
+        if (isUnique(slotPtr, matches))
+            yield slotPtr.map((idx) => array[idx]).reverse()
+    }
+  }
+  
+  
+  
+  /**
+   * Generator that yields all combinations of array items of the given size.
+   *   (No empties, order doesn't matter)
+   * @param {any[]} array - Array containing any type of item
+   * @param {number} size - Size of each combination to return
+   * @returns {any[][]} - Next array of size 'size'
+   */
+  function* getCombinations(array, slotCount) {
+    // Handle invalid array length
+    if (!array?.length) return
+
+    // Initialize pointers for each slot (Most active ptr first)
+    const slotPtr = Array(slotCount).fill(0).map((_,i) => slotCount - i - 1)
+    // Set limits for each pointer
+    const limit = slotPtr.map((_,i) => array.length - i - 1)
+    
+    slotPtr[0]-- // First iteration fix
+    while (true) {
+        // Find first pointer that doesn't need to wrap
+        let wrapSlot = slotPtr.findIndex((ptr, idx) => ptr < limit[idx])
+        if (wrapSlot === -1) break // No more pointers left to wrap
+  
+        // Wrap all upstream pointers
+        while (wrapSlot--) slotPtr[wrapSlot] = ++slotPtr[wrapSlot + 1]
+  
+        // Base increment
+        ++slotPtr[0]
+  
+        // Convert pointers to actual array values
+        yield slotPtr.map((idx) => array[idx]).reverse()
+    }
+  }
 
 
 /** Calculate count of all possible non-repetative combinations of items in slotCount slots, allowing empty slots */
@@ -113,6 +214,7 @@ function hasRepeats(array, ignoreValue) {
 // EXPORTS \\
 module.exports = {
     combinationCount, getCombinationN,
+    getCombinations, getUniqueCombinations,
     permutationCount, getPermutations,
     getArrayCombos,
 }
