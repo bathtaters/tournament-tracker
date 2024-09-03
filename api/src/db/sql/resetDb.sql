504e2e3b42ecc48d18dc32e48f468d3cbb32ac92
Nick C <bathtaters@gmail.com>
2024-09-03 02:14:35 -0400

    create Log DB table/enum

--- api/src/db/sql/resetDb.sql
+++ api/src/db/sql/resetDb.sql
@@ -1,3 +1,5 @@
+-- Initialize DB --
+
 SET sql_safe_updates = FALSE;
 
 USE defaultdb;
@@ -9,6 +11,14 @@ SET sql_safe_updates = TRUE;
 CREATE DATABASE IF NOT EXISTS %DB%;
 
 USE %DB%;
+
+
+-- TYPES/ENUMS --
+
+CREATE TYPE LOG_ACTION AS ENUM ('create', 'update', 'delete', 'login');
+
+
+-- BASE TABLES --
 
 CREATE TABLE settings (
     id STRING PRIMARY KEY NOT NULL,
@@ -87,7 +97,27 @@ CREATE TABLE voter (
     events UUID[] NOT NULL DEFAULT '{}'
 );
 
--- Perms
+CREATE TABLE log (
+    -- Base
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    dbtable STRING,         -- Table that was changed
+    tableid STRING NULL,    -- Row that was changed (Should only be NULL if action = CREATE and error != NULL)
+    action LOG_ACTION DEFAULT 'update', -- Type of change
+    data JSONB NULL,        -- Attempted update to data
+    userid UUID NULL,       -- Who made the change (Nullable to allow deleting users)
+    error STRING NULL,      -- NULL if update succeeded, otherwise error message
+    ts TIMESTAMPTZ DEFAULT current_timestamp(), -- Date/Time of update
+
+    -- Foreign Keys
+    FOREIGN KEY (userid) REFERENCES player(id) ON DELETE SET NULL ON UPDATE CASCADE,
+
+    -- Indexes
+    INDEX time_idx (ts), -- sort
+    INDEX row_idx (tableid) -- filter
+) WITH (ttl_expiration_expression = 'ts + INTERVAL ''1 year''', ttl_job_cron = '@monthly');
+
+
+-- Perms --
 GRANT ALL ON DATABASE %DB% TO db_admin;
 GRANT ALL ON TABLE * TO db_admin;
 GRANT INSERT, UPDATE, DELETE, SELECT ON TABLE * TO db_rw;
