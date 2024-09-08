c427170c7f005f0fc61e47c5c07cfe2a4fdc840d
Nick C <bathtaters@gmail.com>
2024-09-08 13:49:18 -0400

    replace logo with 2024 logo

--- client/src/pages/header/styles/Logo.js
+++ client/src/pages/header/styles/Logo.js
@@ -1,16 +1,15 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
-import logoLight from "../../../assets/images/logo2024_light.png"
-import logoDark from "../../../assets/images/logo2024_dark.png"
+import logo from "../../../assets/images/logo2024.png"
 
 import { defaultSettings } from "../../common/services/fetch.services";
 
 function Logo({ title, to }) {
   return (
     <Link to={to} className="h-full">
-      <img className="h-full w-auto block dark:hidden" src={logoLight} alt={title || defaultSettings.title} />
-      <img className="h-full w-auto hidden dark:block" src={logoDark} alt={title || defaultSettings.title} />
+      <img className="h-full w-auto block" src={logo} alt={title || defaultSettings.title} />
+      {/* <img className="h-full w-auto hidden dark:block" src={logoDark} alt={title || defaultSettings.title} /> */}
     </Link>
   )
 }
