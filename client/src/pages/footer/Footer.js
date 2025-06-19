331d2a76b10ed1e9012a7fe1e8172476e7b61bef
Nick C <bathtaters@gmail.com>
2025-06-18 23:18:31 -0400

    add version to Footer and fix Footer width

--- client/src/pages/footer/Footer.js
+++ client/src/pages/footer/Footer.js
@@ -1,12 +1,13 @@
-import React from "react"
-
 import FooterStyle from "./FooterStyle"
+import { useSettingsQuery } from "../common/common.fetch"
 import { footerText } from "../../assets/constants"
 
 function Footer() {
+  const { data } = useSettingsQuery();
+
   return (
     <FooterStyle>
-      <div><p>{footerText}</p></div>
+      <div><p>{footerText(data.dbversion)}</p></div>
     </FooterStyle>
   )
 }
