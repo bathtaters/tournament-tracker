c3fe3dfc4d16deb1a28918bc67e1e8ec8c20572a
Nick C <bathtaters@gmail.com>
2025-09-05 09:47:26 -0400

    Create model types based on validate.json.

---
+++ api/src/types/models.d.ts
@@ -0,0 +1,88 @@
+export type Settings = {
+  id: string;
+  title: string;
+  showrawjson: boolean;
+  autofillsize: number;
+  autobyes: boolean;
+  dayslots: number;
+  datestart: Date;
+  dateend: Date;
+  planstatus: number;
+  plandates: Date[];
+  planslots: number;
+  planmenu: boolean;
+  planschedule: boolean;
+  showcredits: boolean;
+  showstandings: boolean;
+};
+
+export type Player = {
+  id: string; // uuid
+  name: string;
+  password?: string;
+  credits: number;
+  access: number;
+  session?: string;
+  hide: boolean;
+};
+
+export type Event = {
+  id: string; // uuid
+  title: string;
+  day?: Date;
+  slot: number;
+  plan: number;
+  format: string;
+  team?: string;
+  players: string[]; // uuid[]
+  playercount: number;
+  teamsize: number;
+  roundactive: number;
+  roundcount: number;
+  wincount: number;
+  playerspermatch: number;
+  notes: string;
+  link: string;
+  clocklimit: string; // interval
+  clockstart?: Date;
+  clockmod?: string; // interval
+};
+
+export type Match = {
+  id: string; // uuid
+  eventid: string; // uuid
+  round: number;
+  players: string[]; // uuid[]
+  wins: number[];
+  draws: number;
+  drops?: string[]; // uuid[]
+  reported: boolean;
+  undrop: boolean;
+  playerid: string; // uuid
+};
+
+export type Team = {
+  id: string; // uuid
+  name?: string | null;
+  players: string[]; // uuid[]
+};
+
+export type Voter = {
+  id: string; // uuid
+  days: Date[];
+  events: string[]; // uuid[]
+};
+
+export type SwapItem = {
+  id: string; // uuid
+  playerid: string; // uuid
+};
+
+export type Swap = {
+  swap: SwapItem[];
+};
+
+export type Plan = {
+  voters: string[]; // uuid[]
+  events: string[]; // uuid[]
+};
